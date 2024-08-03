7f9b02ad954c0f835f52256787bcb72b59e57af7
Sandro Zabakhidze <145058114+Sandroz4@users.noreply.github.com>
2024-08-03 15:10:36 +0400

    commit

---
+++ Group 39/day 12/classwork/cw.py
@@ -0,0 +1,15 @@
+# print(True and False)
+# print(False or False)
+
+# print(5 > 5)
+# print(5 >= 5)
+# print(3 != 5)
+# print(5 != 5)
+
+
+
+print('hello')
+
+print('hello')
+print('hello')
+print('hello')
