c8a9f9c69295c7f0c2ca6014a6350b2a8cea6c14
Sandro Zabakhidze <145058114+Sandroz4@users.noreply.github.com>
2024-08-06 22:35:30 +0400

    commit

---
+++ Group 37/day 8/classwork/cw.py
@@ -0,0 +1,78 @@
+# name = 'sandro' #string
+# age = 14 #integer
+# player_health = 0.5 #float
+
+# print(type(name))
+# print(type(age))
+# print(type(player_health))
+
+# name = 'saba'
+
+# print(type(name))
+# print(name)
+
+# print(type('saba'))
+
+# age = '4'
+
+# print(5 + age)
+
+# age = '13'
+
+# print(2 + int(age))
+
+# age = '14'
+# weight = 67
+
+# print(int(age) + weight)
+
+
+# age = 12
+
+# print(float(age))
+
+# age = 14
+# age2 = '6'
+
+# print(str(14) + age2)
+
+# int()
+# str()
+# float()
+
+# age = input()
+# print(type(age))
+
+
+# print(16 / 4) #implicit data conversion
+# print(int('5')) #explicit data conversion
+
+
+# # 1. შექმენით ცვლადი საიდანაც თავიდან იქნება მისი მნიშვნელობა 
+# # 10 შემდეგ გადააქციეთ ის სტრინგად და დაპრინტეთ
+# age = 10
+# print(str(age))
+
+
+# # 2. შექმენით 2 ცვლადი სადაც გექნებათ სტრინგები შენახული, 
+# # შემდეგ გადააგქციეთ ინტეჯერებად
+# age = '3'
+# age2 = '5'
+
+# int(age)
+# int(age2)
+
+# # 3.მომხმარებელს შემოატანინეთ float ტიპის მნიშვნელობა, ჯერ
+# # გაიგეთ მისი ტიპი შემდეგ დაპრინტეთ თვითონ ეს  float
+
+# user_input = float(input())
+
+# print(type(user_input))
+# print(user_input)
+
+# # 4. მომხმარებელს შემოატანინეთ მისის ასაკი, და დაუპრინტეთ
+# # რამდენი წლის იქნება 4 წელში
+
+# age = int(input()) # 16
+
+# print(age + 4) # 20
