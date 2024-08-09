34b13ba8400dc0767eda2fbe0595b7643d0a53ad
Sandro Zabakhidze <145058114+Sandroz4@users.noreply.github.com>
2024-08-09 18:31:17 +0400

    commit

---
+++ Group 39/day 14/classwork/test.py
@@ -0,0 +1,45 @@
+# for i in range(4):
+#     print('hello')
+    
+
+# range(4) - 0, 1, 2, 3
+
+# i = 0, i = 1, i = 2, i = 3
+
+# for i in range(7):
+#     print(i)
+
+# range(7) - 0, 1, 2, 3, 4, 5, 6
+
+# i = 0
+# i = 1
+# i = 2
+# i = 3
+# i = 4
+# i = 5
+# i = 6
+
+# range(4) = 0, 1, 2, 3
+
+# i = 0, 2
+# i = 1, 3
+# i = 2, 4
+# i = 3, 5
+
+# for i in range(4):
+#     print(i+2)
+
+# for i in range(3):
+#     print('hello')
+#     print(i)
+
+# range(4) - 0, 1, 2, 3
+
+
+# for i in range(4):
+#     print(i+1)
+
+# i = 0 
+# i = 1 
+# i = 2 
+# i = 3 
