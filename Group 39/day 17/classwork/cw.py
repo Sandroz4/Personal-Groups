329d49cd90a22b1d5aad7e7d737e7c7ad61fd300
Sandro Zabakhidze <145058114+Sandroz4@users.noreply.github.com>
2024-08-16 16:55:50 +0400

    commit

---
+++ Group 39/day 17/classwork/cw.py
@@ -0,0 +1,64 @@
+# print('hello world')
+# print('hello world')
+# print('hello world')
+
+# for i in range(3):
+#     print('hello world')
+
+# age = 0
+
+# while age < 10:
+#     print('you are a kid')
+#     age = age + 1
+
+
+# age = 30
+
+# while age > 10:
+#     print('you are teenager')
+#     age = age/2
+
+
+# age = 0 
+
+# while age <= 10:
+#     print('hello')
+#     age = age + 1
+
+
+# range(3) -> 0,1,2
+
+# for i in range(3):
+#   print(i < 1) #true, false, false
+
+
+# for box in range(50):
+#   print("Package A")
+#   print("Task complete")
+
+
+
+# number = 10
+# user_guess = int(input('Enter a number: '))
+
+# while user_guess != number:
+#     user_guess = int(input('Enter a number: '))
+
+# print('you guessed right!')
+
+
+# password1 = input('Enter password: ')
+# password2 = input('Please confirm password: ')
+
+# while password1 != password2:
+#     password2 = input('Please confirm password: ')
+
+# print('Password set!')
+
+secret = 'goa'
+user_guess = input('enter secret word: ')
+
+while user_guess != secret:
+    user_guess = input('enter secret word: ')
+
+print('match!')
