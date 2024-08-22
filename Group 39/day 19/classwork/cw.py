604aec0b2c99e33b53e4588d5a288a3af1c2b2f2
Sandro Zabakhidze <145058114+Sandroz4@users.noreply.github.com>
2024-08-22 23:24:05 +0400

    commit

---
+++ Group 39/day 19/classwork/cw.py
@@ -0,0 +1,98 @@
+# if - თუ
+# elif - ან თუ
+# else - სხვა შემთხვევაში
+
+# number = 24
+
+# # თუ რიცხვი ნაკლებია ოცზე
+# if number < 20:
+#     print('Your number is below twenty!')
+# # ან თუ რიცხვი ტოლია ოცის
+# elif number == 20:
+#     print('your number is equal to twenty')
+# # სხვა შემთხვევაში
+# else:
+#     print('your number is more than twenty!')
+
+age = 14
+
+if age >= 18:
+    print("Regular Price")
+else:
+    print("Discount")
+
+
+# print('good')
+
+
+# age = 32
+# is_student = True
+
+# #   False   or    True -> True
+# if age < 18 or is_student:
+#   print("Discount")
+# else: 
+#   print("Regular price")
+
+# age = 32
+
+# # ვაკეთებთ პროგრამას რომელსაც გამოაქვს ტექსტი
+# # თუ ასაკი ნაკლებია 40-ზე
+
+# if age < 40: 
+#     print('underage')
+
+
+# print(True or False) # True
+# print(False or True) # True
+# print(False or False) # False
+
+# print(False and True) # False
+# print(True and True) # True
+# print(False and False) # False
+
+# or - False გამოაქვს მხოლოდ მაშინ,
+# როდესაც ორივე მხარე False-ა
+
+# and - True გამოაქვს მხოლოდ მაშინ, 
+# როდესაც ორივე მხარე True-ა
+
+
+
+
+# print(False or True) #True
+# print(True or False) #True
+# print(False or False) #False (ერთადერთი ვარიანტი)
+# print(True or True) #True
+
+# print(True and True) #True (ერთადერთი ვარიანტი)
+# print(False and True) #False
+# print(False and False) #False
+# print(True and False) #False
+
+
+# #python code
+# age = 14
+
+# while age < 18:
+#     print(age)
+#     age += 1
+
+#pseudo code
+# age=14
+
+# while age < 18
+# printage
+# age +=1
+
+
+
+# #natural language
+# ასაკი = 14
+
+# სანამ ასაკი ნაკლები იქნება 18ზე
+# დაპრინტე underage
+
+for x in range(200):
+  print("Repeat")
+print("Stop")
