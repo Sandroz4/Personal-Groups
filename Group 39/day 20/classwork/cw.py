5270c0beca6706c0b092e0bd80dd78281b0943d1
Sandro Zabakhidze <145058114+Sandroz4@users.noreply.github.com>
2024-08-25 23:25:54 +0400

    commit

---
+++ Group 39/day 20/classwork/cw.py
@@ -0,0 +1,10 @@
+# # 3. Create an if-else statement to determine if a person is a 
+# # teenager. If the age is less than 19 print "You are a teenager!";
+# # otherwise, print "You are not a teenager."
+
+# age = int(input('enter your age: ')) 
+
+# if age <= 19:
+#     print('You are a teenager!')
+# else:
+#     print('You are not a teenager!')
