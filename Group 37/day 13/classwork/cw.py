dddb928fd4a5b8649b3dd6c25cfa1d80782e0a1d
Sandro Zabakhidze <145058114+Sandroz4@users.noreply.github.com>
2024-09-03 22:40:31 +0400

    commit

---
+++ Group 37/day 13/classwork/cw.py
@@ -0,0 +1,58 @@
+# for i in range(3):
+#     print('goa')
+
+
+# range(3) -> 0, 1, 2
+# range(5) -> 0, 1, 2, 3, 4
+
+
+# for i in range(5):
+#     print('hello')
+
+
+# print('hello')
+# print('hello')
+# print('hello')
+# print('hello')
+# print('hello')
+
+# for loop
+
+# for i in range(5):
+#     print('hello')
+
+# 0-იდან 7-ის ჩათვლის ციფრები, for loop-ით
+
+# 0, 1, 2, 3, 4
+
+# for x in range(6):
+#     print(x)
+
+
+#range(3) -> 0, 1, 2
+#range(5) -> 0, 1, 2, 3, 4
+#range(2) -> 0, 1
+
+
+# for i in range(9):
+#     print('hello')
+
+
+# age = 20
+
+# # სანამ 13 ნაკლებია 18-ზე
+# while age < 18:
+#     # მანამდე დაპრინტოს ეს
+#     print('minor')
+
+
+# age = 12
+
+# while age < 18:
+#     print('minor')
+#     age = age + 2
+
+num = 13
+
+while num > 5:
+    print('old')
