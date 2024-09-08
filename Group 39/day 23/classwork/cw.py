dfdf2d4e33f33ffa4c7654fada79a8cc2d2ebef3
Sandro Zabakhidze <145058114+Sandroz4@users.noreply.github.com>
2024-09-08 20:59:28 +0400

    commit

---
+++ Group 39/day 23/classwork/cw.py
@@ -0,0 +1,66 @@
+#                  0        1         2         3           4
+# shopping_cart = ['bread', 'milk', 'cheese', 'sausage', 'coffee']
+
+# print(shopping_cart[1:3])
+# print(shopping_cart[1:4])
+
+
+
+
+# #       012
+# name = 'goa'
+
+# print(name[1])
+
+# print(shopping_cart)
+# print(shopping_cart[1])
+
+#        
+# fruit = 'orange'
+
+# print(fruit[])
+
+# box = 'shoe'
+
+#         0       1         2       3
+# box = ['shoe', 'phone', 'tablet', 'pc']
+
+# print(box[1:3])
+
+#        012345
+# fruit = 'orange'
+
+# print(fruit[0:2])
+
+
+# #           0           1          2            3           4           5
+# list = ['group 37', 'group 39', 'group 40', 'group 41', 'group 42', 'group 43']
+
+# # print(list[:3])
+# print(list[2:])
+
+#                   -6      -5      -4        -3        -2        -1
+# shopping_cart = ['bread', 'milk', 'cheese', 'sausage', 'coffee', 'beans']
+
+# print(shopping_cart[-3:-1])
+
+#                   0       1        2          3          4        5
+
+
+
+
+
+#                   -7     -6        -5      -4         -3        -2         -1
+# shopping_cart = ['bread', 'milk', 'cheese', 'sausage', 'coffee', 'beans', 'sause']
+
+# print(shopping_cart[-7:-4])
+
+
+# c = ['$', '£', '€', '¥']
+
+
+# print(c[-2:])
+
+vehicle = 'airplane'
+vehicle[:3] = 'water'
+print(vehicle)
