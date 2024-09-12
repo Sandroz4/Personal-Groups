2825dff647f2972611697bb4c0b8f3aec61f451b
Sandro Zabakhidze <145058114+Sandroz4@users.noreply.github.com>
2024-09-13 00:15:44 +0400

    commit

---
+++ Group 39/day 25/classwork/cw.py
@@ -0,0 +1,64 @@
+# # immutable (არ შეგვიძლია შევცვალოთ შექმნის შემდეგ)
+# name = 'goa'
+# name[1] = 'e'
+
+# print(name)
+
+
+# # #mutable (შეგვიძლია შევცვალოთ შექმნის შემდეგ)
+# list = ['mic', 'mouse', 'keyboard', 'headset']
+# list[1] = 'monitor'
+
+# print(list)
+
+
+
+
+# list = ['mic', 'mouse', 'keyboard', 'headset']
+
+# #     01234
+# #     mouse
+# print(list[1][3])
+
+
+# list = [['mic', 'mouse'], ['keyboard', 'headset']]
+
+# print(list[0][1][2])
+
+
+
+
+
+
+
+# functions
+
+
+# print('hello')
+
+# age = 15
+
+# print(type(age))
+
+# for i in range(4):
+#     print(i)
+
+# print('goa')
+# print()
+# print('bye')
+
+# print("Your seat:", 4)
+
+# print()
+
+# num = '45' # string
+# print(int(num) + 3)
+
+
+
+# print(2 + int('4') )
+
+
+print("number on that index is " + str([0,1,2,3,4,5,6,7][int(input("enter an index: "))]))
+
+
