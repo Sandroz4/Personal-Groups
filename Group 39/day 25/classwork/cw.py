119ba04bf469c0410c71836d20a75d7f30110e73
Sandro Zabakhidze <145058114+Sandroz4@users.noreply.github.com>
2024-09-15 22:51:33 +0400

    commit

--- Group 39/day 25/classwork/cw.py
+++ Group 39/day 25/classwork/cw.py
@@ -59,6 +59,113 @@
 # print(2 + int('4') )
 
 
-print("number on that index is " + str([0,1,2,3,4,5,6,7][int(input("enter an index: "))]))
+# print("number on that index is " + str([0,1,2,3,4,5,6,7][int(input("enter an index: "))]))
+
+
+
+
+
+
+
+
+
+
+
+# 10. შექმენით რიცხვების list'ი, შექმენით for loop'ი list'ის 
+# რიცხვების ჯამის გამოსათვლელად.
+
+
+# numbers = [2, 4, 3, 1]
+
+# sum = 0
+
+# for i in numbers:
+#     sum += i
+
+
+# print(sum)
+
+
+
+
+
+
+
+
+
+
+# #          G  G  G  G  G
+# numbers = [2, 4, 5, 3, 8]
+# sum = 22
+
+# for i in numbers:
+#     sum = sum + i
+
+# print(sum)
+
+# i ხდება სათითაოდ სიის ყველა ელემენტი.
+
+# i = 2
+# i = 4
+# i = 5
+# i = 3
+# i = 8
+
+
+
+
+# 13. შექმენით while loop'ი რომელიც დათვლის
+#  რიცხვებს 1დან 100მდე თუმცა გამოტოვებს რიცხვებს 50დან 60მდე.
+
+
+# # 14. შექმენით while loop'ი, რომელიც დაიწყებს
+# #  რიცხვების შეკრებას 1-დან, სანამ ჯამი არ გაუტოლდება 50-ს.
+
+# number = 1 #2, 3, 4
+
+# while number < 50:
+#     number = number + 1
+
+# print(number)
+
+
+
+
+# 10, 13, 14
+
+
+
+# list = [2, 5, 8, 9, 13]
+
+
+
+# for x in range(3):
+#     print(x)
+
+
+# range(5) -> 0, 1, 2, 3, 4
+# i = 0
+# i = 1
+# i = 2
+# i = 3
+# i = 4
+
+# range(5) -> 0, 1, 2, 3, 4
+
+# for i in range(5):
+#     print(i)
+
+
+# 0 +2
+# 1 +2
+# 2 +2
+# 3 +2
+# 4 +2
+
+
+
+
+
+
 
 
