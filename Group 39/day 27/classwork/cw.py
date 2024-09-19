482f490682928f8185d3ed2ef02e08e4c7742b76
Sandro Zabakhidze <145058114+Sandroz4@users.noreply.github.com>
2024-09-19 23:28:10 +0400

    commit

---
+++ Group 39/day 27/classwork/cw.py
@@ -0,0 +1,98 @@
+# print('name')
+
+# name = 'goa'
+
+# print(type(name))
+
+# number1 = 0
+# number2 = 1
+
+# print(bool(number1))
+# print(bool(number2))
+
+# number = '1'
+
+
+# print(int(number) + 3)
+
+# name = 'ilia'
+# age = 13
+# level = 34.5
+
+#float - ათწილადი (12.5)
+#int - მთელი რიცხვი (-2, 0, 335)
+#string - ყველაფერი ""-ში
+
+# word = 'dog'
+# count = 6
+
+# print(count)
+
+# print(int(count) + 5)
+
+# print( str(count)   + 3 )
+
+# print('GIORGI'.lower())
+# print('giorgi'.upper())
+
+
+
+#       
+name = 'GRoup'
+
+# .lower() - გადააქცევს ყველა ასოს პატარა ასოდ
+# .upper() - გადააქცევს ყველა ასოს დიდ ასოდ
+
+
+# print( name.lower() )
+# print( name.upper() )
+
+# print(name[0:4] + name[4].upper())
+
+
+# upper(name)
+# name.upper()
+
+
+
+
+# name = 'laptop'
+
+# print( 'gio'.upper() )
+# print( name.lower() )
+# print(name.capitalize())
+
+
+# name = 'anastasia'
+
+# name[1] = 'g'
+
+# print(name)
+
+
+# #         0      1       2
+# list = ['cow', 'dog', 'horse']
+
+# list[1] = 'cat'
+
+# print(list)
+
+# name = 'table'
+
+# didisxeli = name.upper()
+
+
+
+# .lower() - გადააქცევს ყველა ასოს პატარა ასოდ ('BENI' -> 'beni')
+# .upper() - გადააქცევს ყველა ასოს დიდ ასოდ ('beni' -> 'BENI')
+# .capitalize() - გადააქცევს მხოლოდ პირველ ასოს, დიდ ასოდ ('goa' -> 'Goa')
+# .find() - ეძებს სტრინგში იმ ასოს, რომელსაც ფრჩხილებში დავუწერთ
+
+
+
+password = 'ananasisisi'
+
+
+print(password.find('xx'))
+
+
