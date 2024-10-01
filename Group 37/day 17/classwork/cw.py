b7c676920cbc2de77bbbb373de9fe0fefd03bc8a
Sandro Zabakhidze <145058114+Sandroz4@users.noreply.github.com>
2024-10-01 23:11:54 +0400

    commit

---
+++ Group 37/day 17/classwork/cw.py
@@ -0,0 +1,95 @@
+# age = int(input('Enter your age: '))
+
+# if age > 18:
+#     print('you are an adult')
+# elif age < 18:
+#     print('you are underage')
+# else:
+#     print('goa')
+    
+
+
+
+# age = 13
+
+
+# #   true
+# if age == 13:
+#     print('irakli gogiava')
+
+
+
+# is_student = True
+
+
+# #     True
+# if is_student:
+#     print('hello')
+
+
+# age = 16
+
+# if age < 18:print('hello')
+# elif age > 18:print('adult')
+
+
+# if
+# elif
+# elif
+# elif
+# else
+
+
+
+# age = 13
+# cola = False
+
+# if age < 14:
+#     if cola:
+#         print('really bad bro')
+# else:
+#     print('chad')
+
+# if 15 > 14:
+#     print()
+
+
+# age = 80
+# if age < 18: 
+#   print("Junior discount")
+# elif age >= 75: 
+#   print("Senior discount")
+# print("Proceed to payment")
+
+# for i in range(3):
+#   print('irakli')
+
+
+# for irakli in range(200):
+#   print("Repeat")
+#   print("Stop")
+
+
+# lists
+
+# box = 'clown'
+
+# shopping_list = 'milk'
+
+
+# item1 = 'milk'
+
+
+# #                    0        1        2       3
+# favourite_foods = ['meat', 'bread', 'water', 'ice', 5]
+
+# print(favourite_foods)
+
+
+# academy = 'goa'
+
+# prices = [0.59, 2.99, 14.5]
+
+# grades = [10, 9.5, 8, 10, 2]
+
+
