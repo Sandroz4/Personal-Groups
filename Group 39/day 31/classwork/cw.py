b95dd079407943d47ef3f06454c0124689caf344
Sandro Zabakhidze <145058114+Sandroz4@users.noreply.github.com>
2024-10-06 22:59:18 +0400

    commit

---
+++ Group 39/day 31/classwork/cw.py
@@ -0,0 +1,61 @@
+# print('goa')
+# age = '13'
+# print(int(age) + 2)
+# built-in function
+
+# name = 'giorgi'
+
+# print(name)
+
+
+# def giorgi():
+#     print('hello!')
+#     print()
+#     print('Welcome to transylvania')
+
+
+# giorgi()
+
+
+# def farshevangi():
+#     print('beautiful feathers')
+
+
+# farshevangi()
+
+
+
+# #        'goa'
+# def bye(name):
+#     #               'goa'
+#     print('hello ' + name)
+
+
+# bye('goa')
+
+
+
+
+# def user_age(age):
+#     print('you are ' + str(age) + ' years old!')
+
+# user_age( int(input('enter your age: ')) )
+
+# def doubler(num):
+#     print(num * 4)
+
+
+# doubler(3)
+
+
+# def sum(num1, num2):
+#     print(num1 + num2)
+
+
+# sum(4, 7)
+
+
+def multiply(num1, num2):
+    print(num1 * num2)
+
+multiply(4, 5)
