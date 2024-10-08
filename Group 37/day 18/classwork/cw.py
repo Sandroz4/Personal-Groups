9b05133fca0de9d32328d06f20a3ccafcc1989da
Sandro Zabakhidze <145058114+Sandroz4@users.noreply.github.com>
2024-10-08 22:45:51 +0400

    commit

---
+++ Group 37/day 18/classwork/cw.py
@@ -0,0 +1,19 @@
+# # 4. შექმენი პროგრამა, სადაც მომხმარებელს შეეკითხები რამდენი წლით
+# # უნდა დროში წინ მოგზაურობა,
+# # 
+# # შემდეგ შემოატანინე მისი დაბადების თარიღი,
+# # და დაუპრინტე რომელი წელი იქნება, როდესაც მომავალში მისი სასურველი
+# # წლით გადავა.
+
+
+# user_choice = int(input('how many years do you want to travel in future? ')) # 10
+
+# future_year = 2024 + user_choice
+
+# print('when you travel intoo the future, year will be ' + str(future_year))
+
+
+
+
+
+
