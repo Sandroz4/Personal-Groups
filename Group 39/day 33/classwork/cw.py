30663773bd4f08c9712644fa8affccbacb1b8175
Sandro Zabakhidze <145058114+Sandroz4@users.noreply.github.com>
2024-10-13 23:04:15 +0400

    commit

---
+++ Group 39/day 33/classwork/cw.py
@@ -0,0 +1,82 @@
+# # sum of 2 numbers
+
+# def sum_of_two(num1, num2):
+#     sum_nums = num1 + num2
+#     print(sum_nums)
+
+# sum_of_two(10, 17)
+
+
+# # bmi calculator
+
+# # formula:
+# # weight/(height*height)
+
+# def bmi(weight, height):
+#     index = weight/(height*height)
+#     print(index)
+
+# bmi(36, 1.55)
+
+    
+# word = ['giorgi', 'tornike', 'filipe', 'mariami']
+
+# print(word[::-1])
+
+# word = 'giorgi'
+
+# # print(word[::-1])
+
+
+# def palindrome(word):
+#     # racecar   racecar
+#     if word == word[::-1]:
+#         print(True)
+#     else:
+#         print(False)
+
+# palindrome('ana')
+
+
+
+
+# name = 'hidroeleqtrosadguri'
+
+# print(name[::-1])
+
+
+# def sum_of_two(num1, num2):
+#     sum_numbers = num1 + num2
+
+#     return sum_numbers
+
+# sum_of_two(2, 3)
+
+
+
+
+
+# print('enter your bmi')
+# height = float(input('enter height: '))
+# weight = int(input('enter weight: '))
+
+
+# bmi(height, weight)
+
+
+# def greet(name):
+#     print('hello ' + name)
+
+# greet('anastasia')
+
+
+
+# def km_to_mile(km):
+#     mile = km / 1.609 #6.2
+#     return mile
+
+
+# result = km_to_mile(10) 
+
+# print(result)
+
