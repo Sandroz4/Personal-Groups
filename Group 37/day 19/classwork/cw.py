d4ef32cc616e526595ebd836ff1608c0e06f6d6d
Sandro Zabakhidze <145058114+Sandroz4@users.noreply.github.com>
2024-10-20 23:20:15 +0400

    commit

---
+++ Group 37/day 19/classwork/cw.py
@@ -0,0 +1,147 @@
+# name = 'achiko'
+
+# index-ები, ათვლა იწყება 0-იდან.
+
+# #                  0        1        2
+# shopping_cart = ['milk', 'bread', 'cheese']
+
+# print(shopping_cart[0])
+
+
+# #         0  1  2  3   4   5
+# grades = [9, 9, 8, 10, 10, 7]
+
+
+# print(grades[3])
+
+
+# accessories = ['mouse', 'mousepad', 'camera']
+
+# pc_gadget = accessories[2]
+
+# print(pc_gadget)
+
+
+# books = ['ჯაყოს ხიზნები', 'persy jackson', 'anne frank diaries', 'mein camp']
+
+# books[2] = 'it'
+
+# books[3] = 'pet semetary'
+
+# print(books)
+
+
+
+# shopping_cart = ['milk', 'bread', 'cheese', 'soap', 'meat']
+
+# shopping_cart[2] = 'soda'
+# shopping_cart[1] = 'coke'
+# shopping_cart[0] = 'water'
+
+# print(shopping_cart)
+
+
+# words = ["rise", "sun", "glasses"]
+
+# test = words[1]
+
+# words[1] = 'keyboard'
+
+# print(words[0])
+
+# name = "Sarah"
+# age = 34
+# country = "Germany"
+
+# info = [name, age, country]
+
+# print(info[1])
+
+# shopping_car = [22, 33]
+
+# indexing with strings
+
+
+
+# shopping_cart = ['milk', 'bread', 'cheese', 'er', 'meat']
+
+
+# name = shopping_cart[1]
+# age = shopping_car[1]
+
+# full = name + shopping_cart[3]
+
+# print(full)
+
+# # სწორი პასუხი - დიტო ღუბელაძე
+
+
+
+
+
+# #       012345678
+# club = 'di  namo?'
+
+# print(club[2] + club[3])
+
+
+# list = ['car', 'phone']
+
+# list[1] = 'mic'
+
+# print(list)
+
+
+
+
+# name = 'goa'
+
+# name[1] = 'e'
+
+# print(name)
+
+
+
+
+# list = ['lamp', 'mouse']
+
+# list[1] = 'key'
+
+# print(list)
+
+
+# name = 'dachi'
+
+# name[2] = 'z'
+
+# print(name)
+
+
+
+# list = ['lamp', 'tv']
+
+# list[1] = 'display'
+
+# print(list)
+
+# # mutable
+
+# name = 'revazi'
+
+# name[2] = 'x'
+
+# print(name)
+
+# # immutable
+
+# x = 'dialogue'
+
+# print(x[2] + x[5])
+
+
+# #                  0         1       2         3       4         
+# shopping_cart = ['milk', 'bread', 'cheese', 'juice', 'meat']
+
+# print(shopping_cart[3:5])
+
+
---
+++ Group 39/day 35/classwork/cw.py
@@ -0,0 +1,67 @@
+# print('goa')
+
+# age = 14
+
+# print(type(age))
+
+
+# print(int('4'))
+
+
+
+# # function with print example
+# def sum(num1, num2):
+#     addition = num1 + num2
+#     return addition
+
+# result = sum(2, 3)
+
+# print(result)
+
+
+
+
+
+# def sum(num1, num2):
+#     addition = num1 + num2
+#     return addition
+
+# print(sum(2, 3))
+
+
+
+# def sum(num1, num2):
+#     addition = num1 + num2
+#     return addition
+
+
+# print( sum(6, 4) ) 
+
+
+
+# def product(num1, num2):
+#     result = num1 * num2
+#     return result
+
+# print( product(5, 10) )
+
+
+# def rectangle(length, width):
+#     perimeter = 2*(length+width)
+#     area = length * width
+
+#     return perimeter, area
+
+# result1, result2 = rectangle(10, 5) # return perimeter, area
+
+# print(result1)
+# print(result2)
+
+# def greet(name='sandro'):
+#     print('hello ' + name)
+
+
+# greet('giorgi')
+
+
+# greet('giorgi')
