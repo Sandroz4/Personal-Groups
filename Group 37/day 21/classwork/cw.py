48f38c73c4e3d2d1e0252bf8c02ee96392297561
Sandro Zabakhidze <145058114+Sandroz4@users.noreply.github.com>
2024-10-29 21:20:02 +0400

    commit

---
+++ Group 37/day 21/classwork/cw.py
@@ -0,0 +1,12 @@
+#             0         1             2           3            4
+movies = ['titanic', 'spider man', 'batman', 'superman', 'iron man'] 
+
+
+print(movies[2:])
+
+
+
+# print(movies[1:3])
+# print(movies[2:5])
+
+
---
+++ Group 39/day 37/classwork/cw.py
@@ -0,0 +1,104 @@
+# channel = 'GDS'
+
+# # capitalize() - გადააქცევს სიტყვის პირველ ასოს დიდად
+# # goa -> Goa, GOA -> Goa
+
+# print(channel.capitalize())
+
+# # upper() - გადააქცევს სიტყვის ყველა ასოს დიდად
+# # gds -> GDS, GDs -> GDS
+
+# print(channel.upper())
+
+# # lower() - გადააქცევს სიტყვის ყველა ასოს პატარად
+# # Group -> group, GROUP -> group
+
+# print(channel.lower())
+
+
+# word = 'group'
+
+# print(word.find('p'))
+
+
+# age = 55
+
+# print(len(age))
+
+# 2 * (5 + (4*2))
+
+# age = 5
+
+# x = age + 100
+# y = x
+
+# print(len(str(x)))
+
+
+# #                   0        1      2
+# shopping_cart = ['bread', 'milk', 'tea']
+
+# print(shopping_cart[1])
+
+
+
+# movies=["Avatar","Titanic","Alien"]
+
+# movies.append('it')
+# #movies=["Avatar","Titanic","Alien", 'it']
+
+# print(movies)
+
+
+# #         0        1        2
+# food = ['cake', 'burger', 'gum']
+
+
+# food.insert(2, 'water')
+
+# print(food[3])
+
+
+
+
+# food.append('sneakers')
+
+# print(food)
+
+
+# nums = [1, 7, 9]
+
+# nums.append(4)
+
+# print(nums)
+
+
+# movies.insert(2, "Terminator")
+
+# print(movies[3])
+
+
+# for i in range(5):
+#     print(i)
+
+
+# range(5) -> 0, 1, 2, 3, 4
+
+# i = 0
+# i = 1
+# i = 2
+# i = 3
+# i = 4
+
+num1 = 2
+
+for i in range(64):
+    print(num1 * 2)
+
+# 0, 2, 4
+
+# range(3) -> 0,1,2
+
+# i = 0 
+# i = 1 
+# i = 2 
