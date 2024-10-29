499ec394f8a7ab1a47eb803d1cc1f2f7f244f8b7
Sandro Zabakhidze <145058114+Sandroz4@users.noreply.github.com>
2024-10-29 23:05:25 +0400

    commit

--- Group 37/day 21/classwork/cw.py
+++ Group 37/day 21/classwork/cw.py
@@ -1,12 +1,49 @@
 #             0         1             2           3            4
-movies = ['titanic', 'spider man', 'batman', 'superman', 'iron man'] 
-
-
-print(movies[2:])
-
-
-
 # print(movies[1:3])
 # print(movies[2:5])
 
 
+
+# #            -5           -4         -3          -2         -1
+# movies = ['titanic', 'spider man', 'batman', 'superman', 'iron man'] 
+# #            0             1           2          3          4
+
+
+# print(movies[1:3])
+
+
+
+
+
+# #            0            1           2          3           4          5          6
+# movies = ['titanic', 'spider man', 'batman', 'superman', 'iron man', 'flash', 'wonder woman'] 
+
+
+
+# print(movies[0:7:3])
+
+
+# print(movies[-4:-1])
+# print(movies[-5:-3])
+# print(movies[1:-2])
+# print(movies[:])
+
+
+
+# shopping_cart = ['bread', 'milk', 'cheese', 'chocolate']
+
+# # shopping_cart[2] = 'water'
+# shopping_cart[1:3] = ['sprite', 'sausage']
+
+# print(shopping_cart)
+
+# vehicle = 'airplane'
+# vehicle[:3] = 'water'
+# print(vehicle)
+
+
+# print('hello')
+
+# age = '10'
+
+# print(int(age))
