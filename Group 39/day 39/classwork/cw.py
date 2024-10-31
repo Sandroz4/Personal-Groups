6951a716ee0386773cd1bad9be3c6b93c331d502
Sandro Zabakhidze <145058114+Sandroz4@users.noreply.github.com>
2024-10-31 22:50:37 +0400

    commit

---
+++ Group 39/day 39/classwork/cw.py
@@ -0,0 +1,43 @@
+#                   0       1         2         3        4         5          6
+shopping_cart = ['bread', 'milk', 'potatoe', 'water', 'juice', 'sausage', 'lemonade']
+
+# print(shopping_cart[0:7:3])
+# print(shopping_cart[])
+# print(shopping_cart[-3:-1])
+# print(shopping_cart[:1])
+# print(shopping_cart[5])
+
+# 1. ინახავს რამდენიმე ელემენტს, რომლებსაც აქვთ index-ები.
+# 2. შეგვიძლია შევინახოთ სხვადასხვა ინფორმაციის ტიპის მონაცემები.
+# 3. სიაზე შეგვიძლია გამოვიყენოთ indexing,
+#                             positive + 
+#                             negative -
+# 4. სიაზე შეგვიძლია გამოვიყენოთ slicing.
+
+
+
+# 2nd half 
+
+
+# functions
+
+# def square(number):
+#     squared = number * number
+#     return squared
+
+# squared_number = square(4) #16
+
+# print(squared_number)
+
+
+# numbers = [2, 10]
+
+# print(max(numbers))
+# print(min(numbers))
+# print(len(numbers))
+# print(sum(numbers))
+
+
+
+# max, min, len,
+# for loop-ით სიის გადავლა.
