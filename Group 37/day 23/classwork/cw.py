a88c37ac1fab9c14faa1c7a222e6ee1529e79372
Sandro Zabakhidze <145058114+Sandroz4@users.noreply.github.com>
2024-11-12 23:44:50 +0400

    commit

---
+++ Group 37/day 23/classwork/cw.py
@@ -0,0 +1,57 @@
+# #         0       1         2
+# list = ['mic', 'tablet', 'mouse']
+
+# list[1] = 'lamp'
+# list[2] = 'speaker'
+# list[1] = 'cable'
+
+# print(list)
+
+# #       0123456
+# name = 'academy'
+
+# name[5] = 's'
+
+# print(name)
+
+# print(name[5])
+
+
+# brands = ["Honda", "Toyota", "BMW", "Mercedes", 'Buggati']
+
+
+# print(brands[-3:-1])
+
+
+# #    -4   -3   -2   -1
+# c = ['$', '£', '€', '¥']
+
+# print(c[-3:-1])
+
+
+
+# print('hello world')
+# print('hello world')
+# print('hello world')
+# print('hello world')
+# print('hello world')
+# print('hello world')
+# print('hello world')
+
+
+# for i in range(6):
+#     print('hello world')
+
+
+# print('hello world')
+
+
+
+# print('This is number: ', 5)
+
+
+# print(5 <= 5) #math
+
+
+num = '45'
+print(int(num) + 3)
