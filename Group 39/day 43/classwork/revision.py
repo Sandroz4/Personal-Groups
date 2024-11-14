1cdec3e56e11f31957411a5150012df4be59906e
Sandro Zabakhidze <145058114+Sandroz4@users.noreply.github.com>
2024-11-14 21:38:47 +0400

    commit

---
+++ Group 39/day 43/classwork/revision.py
@@ -0,0 +1,51 @@
+# # positive index    0        1      2        3        4
+# shopping_cart = ['bread', 'milk', 'eggs', 'lemon', 'cheese']
+# # negative index   -5       -4      -3       -2       -1
+
+
+# print(shopping_cart[2]) # eggs
+# print(shopping_cart[-3]) # eggs
+# print(shopping_cart[1:3]) # ['milk', 'eggs']
+# print(shopping_cart[-4:-2]) # ['milk', 'eggs']
+
+
+
+
+# # list functions: sum, max, min, len
+
+# grades = [10, 9, 10, 8, 10]
+
+# # print(sum(grades))
+# # print(max(grades))
+# # print(min(grades))
+# # print(len(grades))
+
+
+# append(), insert(), pop()
+
+# #         0   1   2  3   4   5
+# grades = [10, 9, 10, 3,  8, 10]
+
+# თუ გვინდა, რომ გამოვიყენოთ append(), insert(), pop()-დან რომელიმე:
+
+# 1. ჯერ ვწერთ იმ სიის სახელს, რომელზეც ვიყენებთ.
+# 2. შემდეგ კი ვწერთ წერტილს (dot notation), და ვაყოლებთ სასურველ ფუნქციას().
+
+
+# grades.append(14)
+# grades.insert(3, 'apple')
+
+# #           index, element
+# grades.insert(-2, 3) # 10, 9, 10, 3, 10
+
+# grades.insert(-1, 'goa')
+
+# grades.pop(4,2)
+
+
+# print(grades)
+
+
+
+
+
