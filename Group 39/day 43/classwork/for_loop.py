e2b95b55ddd041fcf9d1fe370051f40491d31b40
Sandro Zabakhidze <145058114+Sandroz4@users.noreply.github.com>
2024-11-14 22:50:08 +0400

    commit

---
+++ Group 39/day 43/classwork/for_loop.py
@@ -0,0 +1,73 @@
+# #           0        1        2         3            4
+# names = ['giorgi', 'beni', 'filipe', 'mariami', 'aleqsandre']
+
+# for i in range(4):
+#     print(i)
+
+# range(4) ქმნის რიცხვებს -> 0, 1, 2, 3
+
+
+
+# range(5) -> 0, 1, 2, 3, 4
+
+# i = 0 
+# i = 1 
+# i = 2 
+# i = 3
+# i = 4
+
+# i = 0 -> 2
+# i = 1 -> 3
+# i = 2 -> 4
+# i = 3 -> 5
+# i = 4 -> 6
+
+# for i in range(5):
+#     print(i+2)
+
+
+
+# for i in range(3):
+#     print(i-1)
+
+
+# for i in range(3):
+#     print(i*2)
+
+
+# for i in range(4):
+#     print(i+i)
+
+# for i in range(3):
+#     print(i//2)
+
+
+# for i in range(3, 11):
+#     print(i)
+
+# # range(3, 11) -> 3, 4, 5, 6, 7, 8, 9, 10
+# # range(4, 7) -> 4, 5, 6
+
+
+# for i in range(5, 10):
+#     print(i+2)
+
+# range(5) -> 0,1,2,3,4
+
+
+# for num in range(2, 8, 3):
+#     print(num)
+
+# range(2, 7) -> 2,3,4,5,6
+# range(2, 7, 2) -> 2,4,6
+
+
+# # შექმენი პროგრამა, მომხარებელს შეეკითხე რომელ წელში უნდა მოგზაურობა,
+# # შემდეგ, კი დაუპრინტე ყველა წელი, რაც მანამდე უნდა გაიაროს, ორი წლის გამოტოვებით.
+
+# user_input = int(input('Enter a year you want to travel into: ')) # 2030
+
+# for i in range(2024, user_input, 2):
+#     print(i)
+
+
