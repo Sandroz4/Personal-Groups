2146037fcaff23c190cdab00a124219a28a6a3c8
Sandro Zabakhidze <145058114+Sandroz4@users.noreply.github.com>
2024-11-21 22:12:45 +0400

    commit

---
+++ Group 39/day 45/classwork/cw.py
@@ -0,0 +1,102 @@
+# for i in range(2, 5):
+#     print(i+i)
+
+
+# # 5-იდან 15-მდე
+
+
+
+
+
+
+
+
+# for loop over lists
+
+
+
+
+
+
+# # begginer
+
+# print(friends[0])
+# print(friends[1])
+# print(friends[2])
+# print(friends[3])
+
+# medium
+
+# #             0        1       2      3
+# friends = ['davit', 'giorg', 'nin', 'an']
+
+
+# for i in friends:
+#     print(i+'i')
+
+
+# range(4) -> 0, 1, 2, 3
+
+# friends - 'daviti', 'giorgi', 'demetre', 'tornike'
+
+# i = 'daviti'
+# i = 'giorgi'
+# i = 'demetre'
+# i = 'tornike'
+
+
+
+
+
+# for num in range(4):
+#     print(num)
+
+
+# # range(4) -> 0, 1, 2, 3
+
+# num = 0
+# num = 1
+# num = 2
+# num = 3
+
+
+
+# name = 'goa'
+
+# # unformatted
+# print('i study in academy called ' + name)
+
+# # f string
+# print(f'i study in academy called {name}')
+
+# # i study in academy called goa
+
+
+# words = ['daviti', 'giorgi', 'demetre', 'tornike']
+
+# for name in words:
+#     print(f'my friend is {name}')
+
+
+# name = 'mariami'
+
+# print(name + ' studies in GOA.')
+
+# print(f'{name} studies in GOA')
+
+
+# numbers = [2, 4, 5, 8, 10, 15]
+
+# for number in numbers:
+#     print(f'{number}a')
+
+
+
+# name = 'filipe'
+# age = 12
+
+
+# print(name + ' is a student, he is ' + str(age) + ' years old.')
+# print(f'{name} is a student, he is {age} years old')
+
+
