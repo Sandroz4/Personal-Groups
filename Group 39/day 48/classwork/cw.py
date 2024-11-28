f15b80d340963ffe85a10b6876b86bc72ddc47bb
Sandro Zabakhidze <145058114+Sandroz4@users.noreply.github.com>
2024-11-28 22:52:59 +0400

    commit

---
+++ Group 39/day 48/classwork/cw.py
@@ -0,0 +1,35 @@
+# def multiply(num1, num2):
+#     print(num1*num2)
+
+# result = multiply(5, 10)
+
+# print(result)
+
+
+# def multiply(num1, num2):
+#     return num1 + num2
+
+
+# print(multiply(5, 10)) 
+
+# result = multiply(5, 10) #50
+
+# print(result)
+
+
+# codewars
+
+# 1:
+# https://www.codewars.com/kata/544675c6f971f7399a000e79
+
+# 2:
+# https://www.codewars.com/kata/50654ddff44f800200000004
+
+# 3:
+# https://www.codewars.com/kata/53369039d7ab3ac506000467
+
+# 4:
+# https://www.codewars.com/kata/57356c55867b9b7a60000bd7
+
+
+
