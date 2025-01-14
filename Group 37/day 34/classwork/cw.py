a210897893d9cc19df8842f58c9c6873888fcfaf
Sandro Zabakhidze <145058114+Sandroz4@users.noreply.github.com>
2025-01-14 22:46:21 +0400

    commit

---
+++ Group 37/day 34/classwork/cw.py
@@ -0,0 +1,26 @@
+# def sum(a, b):
+#     return a + b
+#     print('end of the function')
+
+# print(sum(5, 10))
+
+
+# list = [1,2,3,4,3,5]
+
+# list.append(6)
+# list.remove(3)
+
+# print(list)
+
+
+
+# list = [1,2,3,4]
+
+# for num in list:
+    
+
+
+# range(5) -> 0,1,2,3,4
+
+# for loop() ტრიალდება 5-ჯერ
+
