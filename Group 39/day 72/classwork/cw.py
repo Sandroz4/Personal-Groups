97d36db7b8101e02e46a206cb09dd84f663a663b
Sandro Zabakhidze <145058114+Sandroz4@users.noreply.github.com>
2025-02-20 23:08:45 +0400

    commit

---
+++ Group 39/day 72/classwork/cw.py
@@ -0,0 +1,8 @@
+# positive indexing
+#            0          1         2        3
+names = ['anastasia', 'luka', 'filipe', 'david']
+#            -4        -3        -2        -1
+# negative indexing
+
+
+print(names[3])
