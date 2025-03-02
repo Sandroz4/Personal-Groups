aa7d3adaa0cf44277e9ac5fed4ac33061c307223
Sandro Zabakhidze <145058114+Sandroz4@users.noreply.github.com>
2025-03-02 23:03:11 +0400

    commit

---
+++ Group 39/day 75/classwork/cw.py
@@ -0,0 +1,12 @@
+# list = [1, 3, 5, 7]
+
+# # append()
+
+# list.append(9)
+
+# print(list)
+
+age = 12
+age = 15
+
+print(age)
---
+++ Group 39/day 75/classwork/script.js
@@ -0,0 +1,45 @@
+// let student = document.getElementById('name').textContent //<p id="name">Aleqsandre</p>
+// // console.log(student)
+// console.log(student)
+
+// let student = document.getElementById('name').textContent
+
+// // .textContent
+
+// student = 'Anastasia'
+// console.log(student)
+
+
+// let student = document.getElementById('name').textContent
+// student = 'filipe'
+// console.log(student)
+
+// let dessert = "cake";
+// dessert = "ice cream";
+// alert(dessert);
+
+// {
+// let name = 'nino'
+// name = 'giorgi'
+
+// console.log(name)
+// }
+
+// function Something(){
+//     let group = 39
+//     let level = 75
+
+//     console.log(group, level)
+// }
+
+// Something()
+
+let text = document.getElementById('p1')
+console.log(text)
+
+text.style.color = 'green'
+text.style.fontSize = '20px'
+
+
+// text.style.color = 'red'
+// text.style.fontFamily = 'Verdana, Geneva, Tahoma, sans-serif'
