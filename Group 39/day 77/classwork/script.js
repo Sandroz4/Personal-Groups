84c10ec7cbfb3e1fab97330531c98ebb6e3dd43c
Sandro Zabakhidze <145058114+Sandroz4@users.noreply.github.com>
2025-03-09 23:16:00 +0400

    commit

---
+++ Group 39/day 77/classwork/script.js
@@ -0,0 +1,27 @@
+// function colorRed(){
+//     let text = document.getElementById('heading')
+//     text.style.color = 'red'
+// }
+
+// function colorDefault(){
+//     let text = document.getElementById('heading')
+//     text.style.color = 'black'
+// }
+
+// function sizeUp(){
+// let text = document.getElementById('heading')
+//     text.style.fontSize = '50px'
+// }
+
+// // this function changes text size
+// function sizeDown(){
+//     let text = document.getElementById('heading')
+//     text.style.fontSize = '12.5px'
+// }
+
+// // javascript
+
+function borderRadius(){
+    let image = document.getElementById('poster')
+    image.style.borderRadius = '15px'
+}
