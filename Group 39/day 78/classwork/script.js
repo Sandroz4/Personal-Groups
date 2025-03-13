72b1f19f4a28e440a5e7b8bc4b7e010c47fa8979
Sandro Zabakhidze <145058114+Sandroz4@users.noreply.github.com>
2025-03-13 23:02:50 +0400

    commit

---
+++ Group 39/day 78/classwork/script.js
@@ -0,0 +1,14 @@
+// alert('filipe')
+
+// prompt('enter your name')
+
+// let name = prompt('enter your name: ')
+
+// console.log(name)
+
+// let input1 = document.getElementById('t1').value
+
+// console.log(input1)
+
+
+
