cb5aa94b0e310513f75ff5073af4ae8fa4e48231
Sandro Zabakhidze <145058114+Sandroz4@users.noreply.github.com>
2025-03-18 22:57:13 +0400

    commit

---
+++ Group 37/day 43/classwork/cw.py
@@ -0,0 +1,26 @@
+# def count_positives_sum_negatives(arr):
+#     if not arr:
+#         return []
+#     positive = 0
+#     negative= 0
+#     for i in arr:
+#         if i > 0:
+#             positive += 1
+#         elif i < 0:
+#             negative += i
+#     return [positive , negative]
+
+# print(count_positives_sum_negatives([2,3,4,-2,-3,-5]))
+
+# უნდა დავთვალოთ სიაში დადებითი რიცხვების რაოდენობა და უარყოფითი რიცხვების ჯამი.
+
+# დადებითი რიცხვების რაოდენობა: 2, 3, 4. ამ რიცხვების რაოდენობა არის 3.
+# უარყოფითი რიცხვების ჯამი: -2, -3, -5.  ამ რიცხვების ჯამი არის -10.
+
+
+# 10ც - 2 მეტრიანი
+# 10ც - 1 მეტრიანი
+
+# 100, 400, 1500, 300
+# weights = [40, 45, 40, 65, 70]
+# 52
---
+++ Group 39/day 79/classwork/script.js
@@ -0,0 +1,11 @@
+// confirm('Quit the Website?')
+
+// let user = confirm('are you in group 39?')
+// console.log(user)
+
+function checker(){
+    let box = document.getElementById('c1')
+    console.log(box.checked)
+}
+
+checker()
