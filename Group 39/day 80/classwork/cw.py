124b210e93a50a800c26bfab6107e24569dfc175
Sandro Zabakhidze <145058114+Sandroz4@users.noreply.github.com>
2025-03-21 19:40:29 +0400

    commit

---
+++ Group 39/day 80/classwork/cw.py
@@ -0,0 +1,44 @@
+# box = '3464'
+
+# print(box.isdigit())
+
+
+# box2 = 'ab23'
+
+# for i in box2:
+#     print(i.isdigit())
+
+# name = 'he?/34'
+
+# final = ''
+
+# for i in name:
+#     # თუ არ არის i რიცხვი:
+#     if not i.isdigit():
+#         final += i
+
+
+# print(final)
+
+
+# def string_clean(s):
+#     result = ""
+#     for num in s:
+#         if not num.isdigit():
+#             result += num
+#     return result
+
+
+
+
+name = 'groupgoa'
+
+count = 0
+
+for i in name:
+    if i == 'g':
+        count += 1
+    
+print(count)
+
+
