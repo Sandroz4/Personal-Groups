ed956a018b064870bbc958ac05e6a5c6954682bc
Sandro Zabakhidze <145058114+Sandroz4@users.noreply.github.com>
2025-03-27 22:44:59 +0400

    commit

---
+++ Group 39/day 82/classwork/script.js
@@ -0,0 +1,24 @@
+function edit1(){
+    let img = document.getElementById('image')
+
+    img.style.width = '1000px'
+}
+
+function edit2(){
+    let img = document.getElementById('image')
+
+    img.style.width = '250px'
+}
+
+function edit3(){
+    let img = document.getElementById('image')
+
+    img.style.width = '500px'
+}
+
+function edit4(){
+    let img = document.getElementById('image')
+
+    img.style.borderRadius = '15px'
+}
+
