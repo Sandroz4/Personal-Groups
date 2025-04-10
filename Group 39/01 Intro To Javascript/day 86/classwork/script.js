5c2aa708d4cd662c77cc4665c0cea5df49216d5b
Sandro Zabakhidze <145058114+Sandroz4@users.noreply.github.com>
2025-04-10 22:46:08 +0400

    commit

---
+++ Group 39/01 Intro To Javascript/day 86/classwork/script.js
@@ -0,0 +1,31 @@
+// let count = 0 
+
+// // წამოვიღეთ 4-ივე ელემენტი
+// let countDisplay = document.getElementById('count')
+// let decreaseBtn = document.getElementById('decrease')
+// let resetBtn = document.getElementById('reset')
+// let increaseBtn = document.getElementById('increase')
+
+
+
+// function increaseCount() {
+//     count += 1
+//     countDisplay.textContent = count
+// }
+
+
+// function decreaseCount() {
+//     count -= 1
+//     countDisplay.textContent = count
+// }
+
+// function resetCount() {
+//     count = 0
+//     countDisplay.textContent = count
+// }
+
+
+console.log(5 > 4)
+console.log(5 < 4)
+console.log(5 >= 5)
+console.log(3 != 2)
