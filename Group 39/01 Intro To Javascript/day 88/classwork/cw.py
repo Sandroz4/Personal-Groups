4941e7e1c8ca019d36e96aad8b1e11026bae1130
Sandro Zabakhidze <145058114+Sandroz4@users.noreply.github.com>
2025-04-17 23:15:01 +0400

    commit

---
+++ Group 39/01 Intro To Javascript/day 88/classwork/cw.py
@@ -0,0 +1,75 @@
+# split()
+# print(text.upper())
+# print(text.capitalize())
+# name = 'saba'
+# uppered_name = name.upper()
+# print(uppered_name)
+# გახლიჩეთ ეს სტრინგი ორ სიტყვად
+
+
+# default split()
+
+# გახლიჩეთ ეს სტრინგი ორ სიტყვად და დაბეჭდეთ ორივე
+
+# text = 'python programming'
+
+# splitted = text.split()
+
+# 1v
+# print(splitted[0])
+# print(splitted[1])
+
+
+# 2v
+# for word in splitted:
+#     print(word)
+
+
+
+
+# custom separator
+
+
+# text = 'mic,headset,pad'
+
+# fruits = text.split(',')
+
+# print(fruits)
+
+
+# text = 'mic headset pad light plant'
+
+# fruits = text.split(' ', 3)
+
+# print(fruits)
+
+
+# counting words
+
+
+# name = 'giorgi'
+
+
+# # print(len(name))
+# count = 0
+
+# for i in name:
+#     count += 1
+
+# print(count)
+
+
+# text = 'my name is giorgi gagoshidze'
+
+
+# splitted_text = text.split()
+
+# ['my', 'name', 'is', 'giorgi', 'gagoshidze']
+
+# count = 0
+
+# for word in splitted_text:
+#     count += 1
+
+# print(count)
+
