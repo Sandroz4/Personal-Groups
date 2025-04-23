f9e5d90c49445debc7a7ebc3f2bc5e46a5b61f00
Sandro Zabakhidze <145058114+Sandroz4@users.noreply.github.com>
2025-04-23 22:41:37 +0400

    commit

---
+++ Group 39/01 Intro To Javascript/day 89/classwork/cw.py
@@ -0,0 +1,17 @@
+# age = 18
+# name = 'luka'
+
+# print('my name is ' + name)
+# print(f"my name is {name}")
+
+# print()
+
+
+# print('i am ' + str(age) + ' years old!')
+
+# print(f"i am {age + 5} years old!")
+
+number = 5
+
+#         5      times 2 is 10
+print(f"{number} times 2 is {number * 2}")
