f655f904c037194538670bfe94750a8a718235af
Sandro Zabakhidze <145058114+Sandroz4@users.noreply.github.com>
2025-04-24 22:49:57 +0400

    commit

---
+++ Group 39/01 Intro To Javascript/day 90/classwork/script.js
@@ -0,0 +1,46 @@
+// // print numbers from 1 to user input
+
+// let number = Number(prompt('Enter a number: ')) // 5
+
+// for (let i = 1; i < number; i++) {
+//     console.log(i)
+// }
+
+
+// sum of numbers up to user input
+// შემოტანილ ციფრამდე არსებული რიცხვთა ჯამი
+
+//let number = Number(prompt('Enter //Number: ')) // 4
+
+//let sum = 0
+
+//for (let i = 1; i <= number; i++){
+//    sum += i
+//}
+
+//console.log('Sum is', sum)
+
+//let n = Number(prompt('Enter a //number: '))
+
+//let sum = (n * (n + 1))/2
+
+//console.log(sum)
+
+
+// let second = Number(prompt('Enter number to count down: '))
+
+// for (let i = second; i >= 0; i--){
+//     console.log(i)
+// }
+
+// console.log('times up!')
+
+
+// // All even numbers up to n
+// // ყველა ლუწი რიცხვი n-მდე
+
+// let n = Number(prompt('Enter a number: '))
+
+// for (let i = 0; i < n; i += 2) {
+//     console.log(i)
+// }
