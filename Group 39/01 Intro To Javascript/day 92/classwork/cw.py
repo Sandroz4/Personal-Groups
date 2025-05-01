f5de515de0feb9737335fa054b39eafc90ef2556
Sandro Zabakhidze <145058114+Sandroz4@users.noreply.github.com>
2025-05-01 22:42:49 +0400

    commit

---
+++ Group 39/01 Intro To Javascript/day 92/classwork/cw.py
@@ -0,0 +1,52 @@
+# number = 5
+# digits = [5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+
+# amount_of_multiples = 0
+
+# for i in digits:
+#     if i % number == 0:
+#         amount_of_multiples += 1
+
+
+
+# print(amount_of_multiples)
+
+
+
+# numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+# odd = []
+# even = []
+
+# for i in numbers:
+#     if i % 2 == 0:
+#         even.append(i)
+#     else:
+#         odd.append(i)
+
+# print('odd numbers are:')
+# print(odd)
+# print()
+# print('even numbers are')
+# print(even)
+
+
+
+
+
+# 5,
+
+# რომ დავადგინოთ ეს რიცხვი კენტია თუ ლუწი, უნდა დავაკვირდეთ რა ნათშს მოგვცემს 2-ზე გაყოფისას,
+
+# თუ 2-ზე უნაშთოდ იყოფა, ანუ ლუწია
+# სხვა შემთხვევაში კენტი
+
+
+
+
+
+# ლუწი - 2, 4, 6, 8, 10...
+# კენტი - 1, 3, 5, 7, 9...
+
+
+
