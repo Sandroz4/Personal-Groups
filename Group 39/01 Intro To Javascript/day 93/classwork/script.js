e6cc616478a1b7e215feb7c5c1823787bf935a63
Sandro Zabakhidze <145058114+Sandroz4@users.noreply.github.com>
2025-05-04 22:53:31 +0400

    commit

---
+++ Group 39/01 Intro To Javascript/day 93/classwork/script.js
@@ -0,0 +1,69 @@
+// let greeting = document.getElementById('greet')
+
+// greeting.style.color = 'red'
+
+// console.log(greeting)
+
+// let name = 'giorgi' 
+
+// let name = 'sandro'
+// console.log(name)
+
+// let name = 'giorgi'
+// name = 'saba'
+// console.log(name)
+
+// const brand = 'bmw'
+// brand = 'new'
+// console.log(bra)
+
+
+// let age = 15
+// let age = 13
+
+
+const person = {
+    name: 'saba',
+    lastname: 'surmanidze',
+    age: 13
+}
+
+let y = person.lastname
+
+
+let x = person['name']
+
+console.log(x, y)
+
+
+// let x = person.lastname
+// console.log(x)
+// let x = person.name
+// console.log(x)
+// console.log(person.name)
+// let x = person.age
+// console.log(x)
+// console.log(person.age)
+
+
+
+// const things = {
+//     color: 'red',
+//     height: 183,
+//     weight: 50
+// }
+
+
+const hair = {
+    color: 'blue',
+    isstudent: true
+}
+
+const car = {
+    makeYear: 2000
+}
+
+if (car.makeYear > 2000) {
+    console.log('pass')
+}
+
