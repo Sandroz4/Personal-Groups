ce0e7978802e8ab718ca1579004be64abcde30ac
Sandro Zabakhidze <145058114+Sandroz4@users.noreply.github.com>
2025-05-06 23:10:51 +0400

    commit

---
+++ Group 37/day 50/classwork/script.js
@@ -0,0 +1,20 @@
+// let name = 'sandro'
+// console.log(name)
+
+
+// let heading = document.getElementById('text')
+
+// heading.innerHTML = 'gucci'
+
+// console.log(heading)
+
+// heading.style.color = "white"
+// heading.style.fontSize = '50px'
+// heading.style.fontFamily = 'Verdana'
+// heading.style.background = 'red'
+// heading.style.width = '250px'
+
+
+// console.log(heading)
+
+
