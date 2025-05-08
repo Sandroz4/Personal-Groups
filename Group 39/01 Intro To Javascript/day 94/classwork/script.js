a77c4408bbff01d50d8fb3062f54c63e91733427
Sandro Zabakhidze <145058114+Sandroz4@users.noreply.github.com>
2025-05-08 22:43:45 +0400

    commit

---
+++ Group 39/01 Intro To Javascript/day 94/classwork/script.js
@@ -0,0 +1,68 @@
+// 1.შექმენით ობიექტი სახელწოდებით book მინიმუმ ხუთი კუთვნილებით: 
+// name, author, pages, genre და isAvailable.
+// ობიექტის შექმნის შემდეგ, დაბეჭდეთ თითოეული კუთვნილების მნიშვნელობა კონსოლში.
+
+// const book = {
+//     name: 'Gullivers Travels',
+//     author: 'Jonathan Swift',
+//     pages: 330,
+//     genre: 'Adventure',
+//     isAvailable: false
+// }
+
+// console.log(book.name, book.author, book.pages, book.genre)
+
+// if (book.isAvailable === true) {
+//     console.log('Book is available')
+// } else {
+//     console.log('Book is not available!')
+// }
+
+
+
+
+
+// 2. შექმენით ობიექტი სახელად student ისეთი კუთვნილებებით, 
+// როგორებიცაა name, grade, passed და year.
+// შემდეგ if-else გამოყენებით, შეამოწმეთ, არის თუ არა passed - true. 
+// თუ ასეა, დაბეჭდეთ: 'student passed the grade', წინააღმდეგ შემთხვევაში, 
+// დაბეჭდეთ 'student failed the grade'.
+
+
+
+// 3. შექმენით ობიექტი სახელწოდებით movie ისეთი კუთვნილებებით, როგორებიცაა 
+// title, rating, duration, yearReleased და isAnimated.
+// შემდეგ კონსოლში დაბეჭდეთ წინადადება ობიექტის კუთვნილებების გამოყენებით, 
+// მაგალითად: 'The movie [title] is rated [rating] and was released in [yearReleased]'.
+
+
+// const movie = {
+//     title: 'shrek 1',
+//     rating: 7.9,
+//     duration: 1.3,
+//     yearReleased: 2001,
+//     isAnimated: true
+// }
+
+
+// console.log(`The movie title is ${movie.title}, is rated ${movie.rating}, was released in ${movie.yearReleased}`)
+
+
+
+
+
+
+
+
+
+
+
+
+// 4. შექმენით ობიექტი სახელწოდებით phone: brand, model, storage, batteryLife და is5G კუთვნილებებით,
+// გამოიყენეთ if ოპერატორი, რათა შეამოწმოთ, აქვს თუ არა ტელეფონს is5G დაყენებული true-ზე. თუ ასეა, 
+// დაბეჭდეთ 'supports 5G', წინააღმდეგ შემთხვევაში დაბეჭდეთ 'doesn't support 5G'.
+
+// 5. შექმენით ობიექტი სახელწოდებით game, კუთვნილებებით: name, players, online, releaseYear და genre.
+// შემდეგ გამოიყენეთ if-else განაცხადი, რათა შეამოწმოთ, არის თუ არა players კუთვნილების მნიშვნელობა 1-ზე მეტი. 
+// თუ ასეა, დაბეჭდეთ "Multiplayer game", სხვა შემთხვევაში დაბეჭდეთ "Single player game".
+
