64bee944241042db2bccfd45b5199fcddcb04f6e
Sandro Zabakhidze <145058114+Sandroz4@users.noreply.github.com>
2025-05-18 22:51:47 +0400

    commit

---
+++ Group 39/01 Intro To Javascript/day 96/classwork/cw.py
@@ -0,0 +1,11 @@
+# list = [1, 2, 3, 4]
+# average = sum(list) / len(list)
+# print(average)
+# number = 2.9
+# print(  round(number)          )
+
+import math
+
+number = 2.8
+
+print( math.floor(number) )
