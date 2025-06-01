206d5713d3e6a94b847f0c3ee8192b9696ef1d23
Sandro Zabakhidze <145058114+Sandroz4@users.noreply.github.com>
2025-06-01 22:38:59 +0400

    commit

---
+++ Group 39/02 Intermediate Javascript/day 100/classwork/cw.py
@@ -0,0 +1,12 @@
+numbers = [1, 2, 2, 2, 5, 6]
+
+# count = 0
+
+# for i in numbers:
+#     if i == 2:
+#         count += 1
+
+# print(count)
+
+print(numbers.count(2))
+
---
+++ Group 39/02 Intermediate Javascript/day 100/classwork/script.js
@@ -0,0 +1,23 @@
+// setInterval(consoleLogger, 1000)
+
+// function consoleLogger(){
+//     console.log('hello')
+// }
+
+
+// function counter(){
+//     count = document.getElementById('count')
+//     number = Number(count.textContent)
+//     number += 1
+
+//     if (number > 10) {
+//         clearInterval(timer)
+//     }
+//     else{
+//         count.textContent = number
+//     }
+
+// }
+
+// setInterval(counter, 600)
+
