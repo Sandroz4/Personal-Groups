266afd487ac757d1d68ca69beef0d200ed7b9397
Sandro Zabakhidze <145058114+Sandroz4@users.noreply.github.com>
2025-06-03 22:45:05 +0400

    commit

---
+++ Group 37/01 Intro To Javascript/day 54/homework/script.js
@@ -0,0 +1,16 @@
+// შეცვალეთ მოცემული ID-ის მქონე აბზაცის ტექსტის ფერი ყვითლად 
+// JavaScript-ის გამოყენებით.
+
+let box = document.getElementById('greet')
+
+box.align = 'center'
+box.textContent = 'titanic'
+
+box.style.color = 'red'
+box.style.fontSize = '40px'
+box.style.backgroundColor = 'brown'
+
+console.log(box)
+
+
+
