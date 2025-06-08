f552f552f3a3c194e557749b8ea4adfe0325e67d
Sandro Zabakhidze <145058114+Sandroz4@users.noreply.github.com>
2025-06-08 23:17:06 +0400

    commit

---
+++ Group 39/02 Intermediate Javascript/day 102/classwork/script.js
@@ -0,0 +1,83 @@
+// let parent = document.getElementById('main')
+
+// function elementCreator() {
+//     let child = document.createElement('p')
+//     // let text = document.createTextNode('This is a new element')
+//     // child.textContent = 'This is a new element'
+
+//     // child.appendChild(text)
+
+
+//     parent.appendChild(child)
+// }
+
+// function elementReplacer() {
+//     let parent = document.getElementById('main')
+//     let child = document.querySelector('p')
+
+//     let btn = document.createElement('button')
+//     btn.textContent = 'Button'
+//     btn.style.color = 'red'
+
+//     parent.replaceChild(btn, child)
+
+// }
+
+
+
+// function onOver() {
+//     let parent = document.querySelector('#main');
+//     let p = document.querySelector('p');
+
+//     let btn = document.createElement('button');
+//     btn.textContent = 'hello';
+//     parent.replaceChild(btn, p);
+// }
+
+// function onOut() {
+//     let parent = document.querySelector('#main');
+//     let btn = document.querySelector('button');
+
+//     let p = document.createElement('p');
+//     p.textContent = 'hover over this';
+//     btn.onmouseover = onOver; 
+//     btn.onmouseout = onOut;
+
+//     parent.replaceChild(p, btn);
+// }
+
+
+// function onOut() {
+//     console.log(10)
+// }
+
+// function test() {
+//     console.log('this worked')
+// }
+
+
+
+// let btn = document.getElementById('btn')
+
+// //                    დაჭერაზე -> გაეშვას რაღაც ფუნქცია
+// //                    მოვლენა, ფუნქცია
+// btn.addEventListener(  'click',   test )
+// //                     1,         2
+
+
+// function test() {
+//     console.log('this worked')
+// }
+
+
+
+// btn.addEventListener('pointerover', test)
+
+// function handleClick() {
+//     console.log('Clicked')
+// }
+
+
+// let btn = document.getElementById('btn')
+
+// btn.addEventListener('pointerout', handleClick)
