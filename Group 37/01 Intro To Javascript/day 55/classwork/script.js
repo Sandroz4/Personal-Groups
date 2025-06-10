7696127a8a7c4a21e63ff261c391b3e52ed5986b
Sandro Zabakhidze <145058114+Sandroz4@users.noreply.github.com>
2025-06-10 22:42:16 +0400

    commit

---
+++ Group 37/01 Intro To Javascript/day 55/classwork/script.js
@@ -0,0 +1,47 @@
+
+// {
+// let age = 15
+// alert()
+// console.log()
+// }
+
+// let year = 2025
+// console.log(year)
+
+// function currentYear() {
+//     let year = 2025
+//     console.log(year)
+// }
+
+// currentYear()
+
+
+
+// function greet(name) {
+//     console.log('greet')
+// }
+
+// greet()
+// greet()
+
+// function greet
+
+// let element = document.getElementById('child')
+// element.style.color = 'red'
+// element.style.fontSize = '20px'
+
+
+// function colorChanger() {
+//     let heading = document.getElementById('heading')
+//     heading.style.color = 'blue'
+// }
+
+
+function sizeUp() {
+    let image = document.getElementById('img')
+    image.style.width = '120px'
+}
+function sizeDown() {
+    let image = document.getElementById('img')
+    image.style.width = '60px'
+}
