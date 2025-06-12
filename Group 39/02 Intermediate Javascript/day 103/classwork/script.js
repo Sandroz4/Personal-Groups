6762594f210964724261c0e2af6e30ffaa541130
Sandro Zabakhidze <145058114+Sandroz4@users.noreply.github.com>
2025-06-12 22:39:32 +0400

    commit

---
+++ Group 39/02 Intermediate Javascript/day 103/classwork/script.js
@@ -0,0 +1,91 @@
+// დაამატეთ pointerover-ის მოვლენის მსმენელი div-ზე, 
+// რომელიც მაუსის მასზე გადატანისას ფონის ფერს მწვანედ შეცვლის.
+
+
+
+
+
+// function background() {
+//     let main = document.getElementById('main')
+//     main.style.background = 'green'
+// }
+
+// let main = document.getElementById('main')
+
+// function background() {
+//     main.style.background = 'green'
+// }
+
+
+// main.addEventListener('pointerover', background)
+
+// დაამატეთ მოვლენის მსმენელი სურათზე, რომლის 
+// სიგანეც მასზე დაჭერისას  300 პიქსელი გახდება.
+
+// let img = document.getElementById('img')
+
+// function imgResizer1() {
+//     img.style.width = '300px'
+// }
+
+// function imgResizer2() {
+//     img.style.width = '100px'
+// }
+
+// img.addEventListener('pointerdown', imgResizer1) 
+// img.addEventListener('pointerup', imgResizer2)
+
+
+// function sample() {
+//     console.log(10)
+// }
+
+// let text = document.getElementById('text')
+
+// text.addEventListener('click', function(event) {
+//     console.log(event.target.style.color)
+// })
+
+
+
+// function remove1() {
+//     let btn1 = document.getElementById('btn1')
+//     let main = document.getElementById('main')
+//     let child1 = document.getElementById('child1')
+
+//     main.removeChild(child1)
+//     main.removeChild(btn1)
+// }
+
+// function remove2() {
+//     let btn2 = document.getElementById('btn2')
+//     let main = document.getElementById('main')
+//     let child2 = document.getElementById('child2')
+
+//     main.removeChild(child2)
+//     main.removeChild(btn2)
+// }
+
+
+// function removeAll() {
+//     let main = document.getElementById('main')
+//     let btn1 = document.getElementById('btn1')
+//     let btn2 = document.getElementById('btn2')
+//     let child1 = document.getElementById('child1')
+//     let child2 = document.getElementById('child2')
+
+//     main.removeChild(btn1)
+//     main.removeChild(btn2)
+//     main.removeChild(child1)
+//     main.removeChild(child2)
+// }
+
+// function removeAll() {
+//     let parent = document.getElementById('main')
+
+//     while (parent.firstChild) {
+//         parent.firstChild.remove()
+//     }
+// }
+
+
