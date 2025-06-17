df879470df008480f887fbc54bf4024762ef1de7
Sandro Zabakhidze <145058114+Sandroz4@users.noreply.github.com>
2025-06-17 22:48:06 +0400

    commit

---
+++ Group 37/01 Intro To Javascript/day 56/classwork/cw.py
@@ -0,0 +1,43 @@
+# grocery = ['milk', 'water', 'bread', 'nutella']
+
+
+# (2) იპოვე რომელ ინდექსზეა პური - enumerate()
+
+# for index, name in enumerate(grocery):
+#     if name == 'milk':
+#         print('cow milk or lab milk')
+#         print(index)
+
+# print( list(enumerate(grocery)) )
+
+
+# (1) იპოვე რომელ ინდექსზეა პური - .index()
+
+# for i in grocery:
+#     if i == 'milk':
+#         print(grocery.index(i))
+
+
+
+
+
+
+
+
+# for i in grocery:
+#     print(i)
+
+# .index()
+
+# wt = grocery.index('nutella')
+# print(wt)
+
+
+# def century_calc(year):
+#     print((year+99)//100)
+
+# century_calc(int(input('year: ')))
+
+
+# print(int(7.2))
+
