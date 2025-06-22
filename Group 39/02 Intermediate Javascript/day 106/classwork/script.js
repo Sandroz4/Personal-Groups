0bce7342dfb0e75227edcab38aeef5d6b3bb8d14
Sandro Zabakhidze <145058114+Sandroz4@users.noreply.github.com>
2025-06-22 23:00:36 +0400

    commit

---
+++ Group 39/02 Intermediate Javascript/day 106/classwork/script.js
@@ -0,0 +1,22 @@
+// const food = ['bread', 'milk', 'water']
+
+// document.getElementById('main').textContent = food
+
+// food[2] = 'cheese'
+
+// console.log(food)
+
+// console.log(food.length-1)
+
+const numbers = [10, 20, 30, 40]
+
+
+
+for (let i = 0; i <  numbers.length; i ++) {
+    console.log('hello')
+}
+
+
+// i = 0 -> i = 1 -> i = 2 -> i = 3
+
+
