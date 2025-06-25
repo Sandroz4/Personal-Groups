5267123866c1f5366588c7e1c64fb3bab6be1885
Sandro Zabakhidze <145058114+Sandroz4@users.noreply.github.com>
2025-06-25 17:10:34 +0400

    commit

---
+++ Group 37/01 Intro To Javascript/day 57/classwork/script.js
@@ -0,0 +1,33 @@
+// function btnColor() {
+//     let btn = document.getElementById('btn')
+//     btn.style.color = 'red'
+//     btn.style.border = 'none'
+// }
+
+// let cat = 'black'
+// let Cat = 'white'
+// let CAT = 'boot'
+
+// console.log(cat, Cat, CAT)
+
+
+
+
+// btnColor()
+
+
+// let name = prompt()
+
+// console.log(name)
+
+
+function getInfo() {
+    let userInp = document.getElementById('go')
+    console.log(userInp.value)
+}
+
+
+function getP() {
+    let p = document.getElementById('p')
+    console.log(p.textContent)
+}
