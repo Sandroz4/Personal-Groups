54a63fcd8812a0edb05a23934981e7348ffa126c
Sandro Zabakhidze <145058114+Sandroz4@users.noreply.github.com>
2025-06-29 23:52:26 +0400

    commit

---
+++ Group 39/02 Intermediate Javascript/day 108/classwork/script.js
@@ -0,0 +1,57 @@
+// let box = document.querySelector('h1')
+// console.log(box)
+
+// let boxes = document.getElementsByTagName('h1')
+// let firstElem = boxes[0]
+// let secondElem = boxes[1]
+// console.log(firstElem, secondElem)
+// console.log(boxes[boxes.length - 1])
+
+// const headings = document.getElementsByTagName('h1')
+
+// function turnRed() {
+//     for (let i = 0; i < headings.length; i++) {
+//     // console.log(headings[i])
+//     headings[i].style.color = 'red'
+//     // console.log(headings[i].textContent)
+// }
+// }
+
+
+// let box = document.querySelector('h1')
+// console.log(box.textContent)
+
+// const box = document.getElementsByClassName('grade')
+// console.log(box[0])
+
+
+// let box = document.getElementById('box')
+// let pocket = document.getElementById('pocket')
+
+// const items = box.getElementsByTagName('p')
+
+// for (let i = 0; i < items.length ; i++) {
+//     pocket.appendChild(items[i])
+//     // console.log(items[i])
+// }
+
+
+// for (let i = 1; i <= 10; i++) {
+//     if (i % 2 === 0) {
+//         console.log(i + ' is even')
+//     } else {
+//         console.log(i + ' is odd')
+//     }
+// }
+
+// function sendReport() {
+//     const input = document.getElementsByTagName('input')
+
+//     for (let i = 0; i < input.length; i++) {
+//         console.log(input[i].value)
+//     }
+// }
+
+let ball = document.getElementById('ball')
+let position = 50
+ball.style.left = '50px'
