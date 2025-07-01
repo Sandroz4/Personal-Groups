eb7837ad0ae7364f2576242ac5f6bd467f14695c
Sandro Zabakhidze <145058114+Sandroz4@users.noreply.github.com>
2025-07-01 22:47:51 +0400

    commit

---
+++ Group 37/01 Intro To Javascript/day 58/classwork/script.js
@@ -0,0 +1,36 @@
+// let age = 18
+
+// Number, != integer
+
+// console.log(18 + 18)
+// console.log(18 * 18)
+// console.log(18 - 18)
+// console.log(18 / 18)
+
+// let age = 18
+// console.log(age * 2)
+
+// let name = prompt()
+// console.log(name)
+
+// function user() {
+//     let input = document.getElementById('inp')
+//     console.log(input.value)
+// }
+
+// // == არამკაცრი შედარება, მხოლოდ მნიშვნელობა აინტერესებს
+// // არა ინფორმაციის ტიპი
+// console.log('5' == 5)
+
+// // === მკაცრი შედარება, მნიშვნელობა და ინფორმაციის ტიპი
+// // აინტერესებს
+
+// console.log(5 === 5)
+
+// let inp = confirm('are you an adult?')
+// console.log(inp)
+
+function user() {
+    let input = document.getElementById('inp')
+    console.log(input.checked)
+}
