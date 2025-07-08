fc2cfc563f4ccc32d5c583bd3d0d6dcfb1b62d51
Sandro Zabakhidze <145058114+Sandroz4@users.noreply.github.com>
2025-07-08 22:44:59 +0400

    commit

---
+++ Group 37/01 Intro To Javascript/day 59/classwork/script.js
@@ -0,0 +1,63 @@
+// let element = document.getElementById('main')
+// element.style.color = 'red'
+
+// h1.textContent = 'group'
+// h1.align= 'center'
+// h1.style.color ='blue'
+
+// // console.log(document.body.h1)
+
+
+// document.title = 'my first'
+// alert(document.title)
+
+
+// document.title = 'ro'
+
+
+// let option = document.getElementById('option')
+
+// let isChecked = option.checked
+
+// console.log(option)
+// console.log(isChecked)
+
+// true and false   false
+// true and true    true
+// false and false   false
+
+
+// true or true   true 
+// true or false  true
+// false or true  true
+// false or false    false
+
+
+
+
+
+// and -> &&
+// or - ||
+
+// console.log(    false && false     )
+
+
+// console.log(    true || true  )
+
+
+// let name = 'goa'
+// let age = 15
+// let isStudent = true 
+
+
+// console.log(typeof name)
+// console.log(typeof age)
+// console.log(typeof isStudent)
+
+let age = 15
+// console.log(typeof age)
+
+// console.log(String(age))
+
+let age1 = String(age)
+console.log(typeof age1)
---
+++ Group 39/02 Intermediate Javascript/day 110/classwork/script.js
@@ -0,0 +1,43 @@
+// function logger(event) {
+//     console.log(event.type)
+// }
+
+// nesting
+
+// function outerFunction(arg) {
+//     console.log('outer function has been called')
+    
+
+
+//     function innerFunction() {
+//         console.log('inner function has been called ' + arg)
+//     }
+
+//     innerFunction()
+// }
+
+// outerFunction('test')
+
+
+
+// function greet(arg) {
+//     console.log('hello ' + arg)
+// }
+
+// greet('gio')
+
+
+// window.onload = function() {
+//     console.log(5)
+// }
+
+// const messsages = ['hello', 'how are you?', 'enjoy the site', 
+//     'copyright']
+// const div = document.getElementById('container')
+
+// for (let i = 0; i < messsages.length; i++) {
+//     const textElement = document.createElement('p')
+//     textElement.textContent = messsages[i]
+
+//     div.appendChild(textElement)
+// }
