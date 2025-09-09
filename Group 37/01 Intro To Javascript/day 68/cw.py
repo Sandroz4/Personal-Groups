8bfd6449d0a2e9ddf8e801654a41d30456e44fee
Sandro Zabakhidze <145058114+Sandroz4@users.noreply.github.com>
2025-09-09 22:34:30 +0400

    commit

---
+++ Group 37/01 Intro To Javascript/day 68/cw.py
@@ -0,0 +1,31 @@
+# sentence = 'hello my name is'
+
+# splitted = sentence.split()
+# reversed = splitted[::-1]
+
+# result = ' '.join(reversed)
+
+# print(result)
+
+
+# list1 = [1,2,3]
+# list2 = [-4, -3, 2]
+
+# result = list1 + list2
+
+# print(sorted(set(result)))
+
+
+# list = [3, 2, 5, 100, -4, -50]
+
+
+
+# result = sorted(list)
+
+# print(result)
+
+total = 0
+
+for i in range(3, 10, 3):
+    total += i 
+print(total)
