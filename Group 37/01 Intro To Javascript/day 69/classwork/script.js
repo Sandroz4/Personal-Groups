dee02d8873a5167ff31e0b8593a11a9c2bca5445
Sandro Zabakhidze <145058114+Sandroz4@users.noreply.github.com>
2025-09-16 23:01:27 +0400

    commit

---
+++ Group 37/01 Intro To Javascript/day 69/classwork/script.js
@@ -0,0 +1,18 @@
+// function logFive(num) {
+//     console.log(num)
+// }
+
+
+///   h1
+// const element = document.getElementById('heading')
+
+// element.style.color = 'red'
+// element.textContent = 'i was a text'
+
+
+// document.getElementById('pic').src = 'image.png'
+
+const element = document.getElementById('heading')
+
+
+element.style.color = 'green'
