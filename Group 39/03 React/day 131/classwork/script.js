b0aa186ca8cf9d5d4fafb0263dd94d599ec92a39
Sandro Zabakhidze <145058114+Sandroz4@users.noreply.github.com>
2025-09-18 23:14:05 +0400

    commit

---
+++ Group 39/03 React/day 131/classwork/script.js
@@ -0,0 +1,90 @@
+// let number = 7
+
+// if (number > 5) {
+//     console.log('greater than 5')
+// } else {
+//     console.log('lower than 5')
+// }
+
+// let number = 7 
+
+// //            პირობა         თუ პირობა მართალია   თუ პირობა არ არის მართალი
+// let result = number > 5      ? 'greater than 5'        : 'lower than 5'
+
+// console.log(number > 5 ? 'you won' : 'you lost')
+
+// console.log()
+
+
+// begginer 
+
+// let number = 5
+// let result;
+
+// if (number %2 === 0) {
+//     result = 'Even'
+// } else {
+//     result = 'Odd'
+// }
+
+
+// ternary operator
+
+// let number = 6
+// console.log(number %2 === 0 ? 'Even' : 'Odd')
+
+
+
+// 0 -> 10
+
+// let x = Math.floor(Math.random() * 10)
+
+// console.log(x)
+
+
+// let number = 3.99
+
+// console.log(Math.floor(number))
+
+
+function coinToss () {
+
+  return Math.random() < 0.5 ? 'heads' : 'tails'
+}
+
+// if coinToss() is 'heads' expression should return 'kitty' 
+// and  if coinToss() is 'tails' then 'doggy'
+
+const pics = {
+  kitty: 'https://content.codecademy.com/courses/React/react_photo-kitty.jpg',
+  doggy: 'https://content.codecademy.com/courses/React/react_photo-puppy.jpeg'
+};
+
+let result = pics['kitty']
+console.log(result)
+
+
+// console.log(result)
+
+
+
+
+
+// const person = {
+//     name: 'sandro',
+//     color: 'red'
+// }
+
+// console.log(person.name)
+
+// გუნდი 1 - მარიამ ჩაიძე
+// ნინო ღვინიაშვილი
+// მარიამ გელაშვილი
+
+// გუნდი 2 - გიორგი გაგოშიძე
+// სალომე მოსეშვილი
+// ფილიპე ბურჭულაძე
+
+// გუნდი 3 - საბა ღლიღვაშვილი 
+// სანან მამედოვი
+// საბა სურმანიძე
