6f6fd6ea058d0876dbafd14d9902b0093beb0844
Sandro Zabakhidze <145058114+Sandroz4@users.noreply.github.com>
2025-09-25 22:43:01 +0400

    commit

---
+++ Group 39/03 React/day 133/classwork/script.js
@@ -0,0 +1,58 @@
+
+
+// for (let i = 0; i < numbers.length; i++) {
+//     console.log(numbers[i])
+// }
+
+// numbers.map(function(number, index) {
+//     console.log(number, index)
+// })
+
+// let new_list = []
+
+// numbers.map(function(number) {
+//     new_list.push(number * 2)
+// })
+
+// console.log(new_list)
+
+
+// let names = ['luka', 'saba', 'mariami', 'giorgi']
+
+// names.map(function(name) {
+//     console.log('hello ' + name)
+// })
+
+
+
+// const users = [
+//     { name: 'gio', age: 18},
+//     { name: 'luka', age: 15},
+//     { name: 'levani', age: 28}
+// ]
+
+
+// users.map(function(user) {
+//     console.log(user.name, user.age)
+// })
+
+// let numbers = [2, 4, 6, 8]
+
+// let newList = []
+
+// numbers.map( (number, index) => newList.push(number * 2))
+
+// console.log(newList)
+
+// function(number, index) {
+    // console.log(number)
+// }
+
+
+let names = ['luka', 'saba', 'mariami', 'giorgi']
+
+names.map((name, index) => console.log(name, name.length, index) )
+
+
+
+ 
