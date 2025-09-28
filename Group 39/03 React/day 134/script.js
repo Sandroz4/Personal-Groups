27481bc68726913ba1a790b2a1a724036a92fc79
Sandro Zabakhidze <145058114+Sandroz4@users.noreply.github.com>
2025-09-28 22:41:04 +0400

    commit

---
+++ Group 39/03 React/day 134/script.js
@@ -0,0 +1,17 @@
+const names = ['gio', 'saba', 'luka']
+
+// console.log(names.map(name))
+
+// names.map((name) => console.log(name))
+
+
+// names.map(     function(name, index) {
+//     console.log(name, index)
+// }     )
+
+
+names.map( name => console.log(name))
+
+names.map( function(name) {
+    console.log(name)
+})
