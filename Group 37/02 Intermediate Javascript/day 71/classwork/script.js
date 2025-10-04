f81e01b86ae54fdc877c7094b3753655393bc1b1
Sandro Zabakhidze <145058114+Sandroz4@users.noreply.github.com>
2025-10-04 14:56:59 +0400

    commit

---
+++ Group 37/02 Intermediate Javascript/day 71/classwork/script.js
@@ -0,0 +1,69 @@
+// function removeChildren() {
+//     let container = document.getElementById('container')
+//     let button = document.getElementById('btn')
+
+
+
+//     container.removeChild(button)
+
+// }
+
+
+
+// function replace() {
+//     let container = document.getElementById('container')
+
+//     // ძველი ელემენტი
+//     let button = document.getElementById('btn')
+
+
+//     // ახალი ელემენტი
+//     let heading = document.createElement('h1')
+//     let text = document.createTextNode('hello world')
+//     heading.appendChild(text)
+
+
+//     // ძველს ვცვლი ახლით
+//     container.replaceChild(heading , button)
+
+// }
+
+
+
+// function over() {
+//     console.log('paragraph was overed')
+// }
+
+
+
+// let button = document.getElementById('btn')
+
+// button.onclick = function() {
+//     console.log('button clicked')
+// }
+
+
+// გუნდი 1 - დიტო ღუბელაძე
+// ალექსი დოლიძე
+// ზურა დავითაძე
+
+// გუნდი 2 - თიკა ქათამაძე
+// ოლიკო გოგლიძე
+// დაჩი ბოლქვაძე
+
+// გუნდი 3 - მათე მაისურაძე
+// ნიკოლოზ ფრანგიშვილი
+// რევაზ ფერაძე
+
+
+let button = document.getElementById('btn')
+
+button.onclick = function() {
+    console.log(5)
+}
+
+
+
+
+
+
