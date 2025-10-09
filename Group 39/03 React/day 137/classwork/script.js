d71213d60d04a80cf202fb83781e1c84ed37406a
Sandro Zabakhidze <145058114+Sandroz4@users.noreply.github.com>
2025-10-09 23:21:26 +0400

    commit

---
+++ Group 39/03 React/day 137/classwork/script.js
@@ -0,0 +1,45 @@
+fetch('https://fakestoreapi.com/products/1')
+    .then(response => response.json())
+    .then(data => console.log(data.id))
+    console.log(data.id)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// numbers.map(number => console.log(number))
+
+// numbers.map(function(item, index) {
+//     console.log(item, index)
+// })
+
+
+
+
+// const numbers = [1, 2, 3, 4, 5]
+
+// numbers.map(function(item, index) {
+//     console.log(item, index)
+// })
+
+// numbers.map((item, index) => console.log(item, index))
+
+
+
+// addNumber(10)
+
+// function addNumber(item) {
+//     console.log(item)
+// }
