5c0abef70d5729b66499bbadd221c8314779f034
Sandro Zabakhidze <145058114+Sandroz4@users.noreply.github.com>
2025-10-11 14:46:08 +0400

    commit

---
+++ Group 44,45,46/day 114/classwork/cw.py
@@ -0,0 +1,8 @@
+# def multiply(a, b):
+#     return a * b
+
+# multiply(2, 3)
+
+# numbers = [1, 2, 3]
+
+# numbers.append()
---
+++ Group 44,45,46/day 114/classwork/script.js
@@ -0,0 +1,48 @@
+fetch('https://fakestoreapi.com/products/2')
+    .then(res => res.json())
+    .then(final => console.log(final.title))
+
+
+
+// const numbers = [1, 2, 3, 4, 5]
+
+// numbers.map(function(item) {
+//     console.log(item)
+// })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const numbers = [1, 2, 3, 4, 5]
+
+// numbers.map((item, index) => console.log(item, index))
+
+// numbers.map(function(item, index) {
+//     console.log(item, index)
+// })
+
+// numbers.map((item, index) => console.log(item, index))
+
+// function(){
+
+// }
