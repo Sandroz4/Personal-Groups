1093b36c1017e72412d6a0ffa0ff8b0a9afdf8e9
Sandro Zabakhidze <145058114+Sandroz4@users.noreply.github.com>
2025-10-12 22:26:45 +0400

    commit

--- Group 39/03 React/day 137/classwork/script.js
+++ Group 39/03 React/day 137/classwork/script.js
@@ -8,7 +8,7 @@
 
 //         h1.textContent = product.title
 //         img.src = product.image
-//         p.textContent = product.price
+//         p.textContent = `${product.price}$`
 
 //     })
 
@@ -17,18 +17,51 @@
 
 
 
+// let name = 'sandro'
+// // let greeting = 'hello ' + name
+
+// let greeting = `hello ${name}`
+// console.log(greeting)
+
+
+// const productBox = document.getElementById('productBox')
+
+// fetch('https://fakestoreapi.com/products/3')
+//     .then(response => response.json())
+//     .then(product => {
+//         productBox.innerHTML = `
+//             <div>
+//                 <h1>${product.title}</h1>
+//                 <img src="${product.image}"> 
+//                 <p>${product.price}$</p>
+                
+//             </div>
+//         `
+//     })
+
+
+// fetch('https://fakestoreapi.com/products')
+//     .then(response => response.json())
+//     .then(products => {
+//         const container = document.getElementById('container')
+
+//         const picture = products.map(product => {
+//             return `
+//             <div>
+//             <img src="${product.image}">
+//             <p>${product.title}</p>
+//             </div>`
+//         })
+
+//         container.innerHTML = picture
+//     })
 
 
 
 
+// const numbers = [1, 2, 4]
 
-
-
-
-
-
-
-
+// numbers.map(item => console.log(item))
 
 
 
---
+++ Group 39/03 React/day 138/classwork/script.js
@@ -0,0 +1,68 @@
+// fetch('https://fakestoreapi.com/products/3')
+//     .then(response => response.json())
+//     .then(function(product) {
+//         const img = document.getElementById('img')
+//         const h1 = document.getElementById('heading')
+//         const p = document.getElementById('p')
+
+
+//         h1.textContent = product.title
+//         img.src = product.image
+//         p.textContent = `${product.price}$`
+
+//     })
+
+
+
+
+
+
+// let name = 'sandro'
+// // let greeting = 'hello ' + name
+
+// let greeting = `hello ${name}`
+// console.log(greeting)
+
+
+// const productBox = document.getElementById('productBox')
+
+// fetch('https://fakestoreapi.com/products/3')
+//     .then(response => response.json())
+//     .then(product => {
+//         productBox.innerHTML = `
+//             <div>
+//                 <h1>${product.title}</h1>
+//                 <img src="${product.image}"> 
+//                 <p>${product.price}$</p>
+                
+//             </div>
+//         `
+//     })
+
+
+// fetch('https://fakestoreapi.com/products')
+//     .then(response => response.json())
+//     .then(products => {
+//         const container = document.getElementById('container')
+
+//         const picture = products.map(product => {
+//             return `
+//             <div>
+//             <img src="${product.image}">
+//             <p>${product.title}</p>
+//             </div>`
+//         })
+
+//         container.innerHTML = picture
+//     })
+
+
+
+
+// const numbers = [1, 2, 4]
+
+// numbers.map(item => console.log(item))
+
+
+
+
