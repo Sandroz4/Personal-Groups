285532c68528714f480c2623064475820e311868
Sandro Zabakhidze <145058114+Sandroz4@users.noreply.github.com>
2025-10-12 21:20:32 +0400

    commit

--- Group 39/03 React/day 137/classwork/script.js
+++ Group 39/03 React/day 137/classwork/script.js
@@ -1,7 +1,16 @@
-fetch('https://fakestoreapi.com/products/1')
-    .then(response => response.json())
-    .then(data => console.log(data.id))
-    console.log(data.id)
+// fetch('https://fakestoreapi.com/products/3')
+//     .then(response => response.json())
+//     .then(function(product) {
+//         const img = document.getElementById('img')
+//         const h1 = document.getElementById('heading')
+//         const p = document.getElementById('p')
+
+
+//         h1.textContent = product.title
+//         img.src = product.image
+//         p.textContent = product.price
+
+//     })
 
 
 
@@ -12,6 +21,63 @@ fetch('https://fakestoreapi.com/products/1')
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const products = [
+//   {
+//     "id": 1,
+//     "title": "Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops",
+//     "price": 109.95,
+//     "description": "Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday",
+//     "category": "men's clothing",
+//     "image": "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_t.png",
+//     "rating": {
+//       "rate": 3.9,
+//       "count": 120
+//     }
+//   },
+//   {
+//     "id": 2,
+//     "title": "Mens Casual Premium Slim Fit T-Shirts ",
+//     "price": 22.3,
+//     "description": "Slim-fitting style, contrast raglan long sleeve, three-button henley placket, light weight & soft fabric for breathable and comfortable wearing. And Solid stitched shirts with round neck made for durability and a great fit for casual fashion wear and diehard baseball fans. The Henley style round neckline includes a three-button placket.",
+//     "category": "men's clothing",
+//     "image": "https://fakestoreapi.com/img/71-3HjGNDUL._AC_SY879._SX._UX._SY._UY_t.png",
+//     "rating": {
+//       "rate": 4.1,
+//       "count": 259
+//     }
+//   },
+// ]
 
 
 
