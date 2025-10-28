8765df837315d902d7b62cbe4732851b4e14fb18
Sandro Zabakhidze <145058114+Sandroz4@users.noreply.github.com>
2025-10-28 22:50:36 +0400

    commit

--- Group 44,45,46/day 118/classwork/script.js
+++ Group 44,45,46/day 118/classwork/script.js
@@ -1,28 +1,28 @@
-const film = document.querySelector('input');
-const movieDiv = document.getElementById('movieDiv');
+// const film = document.querySelector('input');
+// const movieDiv = document.getElementById('movieDiv');
 
-function movieFinder() {
-    const filmValue = film.value;
-
-
-    fetch(`https://api.themoviedb.org/3/search/movie?api_key=38c8a267e25940bf35aa75921cdd6af2&query=${encodeURIComponent(filmValue)}`)
-        .then((res) => res.json())
-        .then((movie) => {
-
-            const results = movie.results;
-            const movieData = results[0];
+// function movieFinder() {
+//     const filmValue = film.value;
 
 
-            movieDiv.innerHTML = `
-                <h2 id='heading_2'>${movieData.orignal_title} (${movieData.release_date})</h2>
+//     fetch(`https://api.themoviedb.org/3/search/movie?api_key=38c8a267e25940bf35aa75921cdd6af2&query=${encodeURIComponent(filmValue)}`)
+//         .then((res) => res.json())
+//         .then((movie) => {
+
+//             const results = movie.results;
+//             const movieData = results[0];
+
+
+//             movieDiv.innerHTML = `
+//                 <h2 id='heading_2'>${movieData.orignal_title} (${movieData.release_date})</h2>
                 
-                <img id='img1' src="https://image.tmdb.org/t/p/w1280/${movieData.backdrop_path}" alt="">
-                <img src="https://image.tmdb.org/t/p/w400/${movieData.poster_path}" alt="">
-                <button>${movieData.popularity}</button>
-                <p>${movieData.overview}</p>
-            `;
-        });
-}
+//                 <img id='img1' src="https://image.tmdb.org/t/p/w1280/${movieData.backdrop_path}" alt="">
+//                 <img src="https://image.tmdb.org/t/p/w400/${movieData.poster_path}" alt="">
+//                 <button>${movieData.popularity}</button>
+//                 <p>${movieData.overview}</p>
+//             `;
+//         });
+// }
 
 
 
@@ -41,3 +41,21 @@ fetch(`https://api.openweathermap.org/data/2.5/weather?q=
 
 
 
+const key = "1fab308981764501b85c1a214ded4756"
+const city = "Sudan";
+
+fetch(https://api.openweathermap.org/data/2.5/weather?q=
+    ${encodeURIComponent(city)}&appid=${key}&units=metric)
+    .then(res => res.json())
+    .then(data => {
+        
+
+        resultDiv.innerHTML += `
+        <div class="productDiv">
+            <img src=${data.deg} width="100"></img>
+            <h2>${data.gust}</h2>
+            <b>$${data.speed}</b>
+        </div>`
+    } 
+
+         )
