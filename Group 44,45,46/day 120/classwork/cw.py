b01bc13530213bc27ee0b2b7fd679b95bc0ae474
Sandro Zabakhidze <145058114+Sandroz4@users.noreply.github.com>
2025-11-01 14:50:06 +0400

    commit

---
+++ Group 44,45,46/day 120/classwork/cw.py
@@ -0,0 +1,42 @@
+# names = ['sandro', 'nerse', 'luka', 'giorgi']
+
+
+
+# # გამოიტანეთ ელემენტი მხოლოდ იმ შემთხვევაში, თუ მისი ინდექსი არის კენტი
+
+# for index, name in enumerate(names):
+#     if index %2 == 0:
+#         print(index**2)
+
+
+
+
+
+# numbers = [1, 3 , 5 , 7 , 9, 11, 13 ,15]
+
+
+# #    ინდექსი ელემენტი
+# for  index,  number   in enumerate(numbers):
+#     if index %2 == 0:
+#         print(number)
+#     else:
+#         print(number**2)
+
+
+
+# def check():
+#     return 5 > 2
+
+
+# print(check())
+
+
+
+# print(5%2)
+    # print(name, index)
+    # if name == 'nerse' and index == 1:
+    #     print(True)
+    # 
+    # 
+    # # for i in range(len(names)):
+#     print(names[i], i)
