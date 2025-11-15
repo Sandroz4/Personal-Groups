1b742cd6910ec833b58bd331b3eac3c550082c62
Sandro Zabakhidze <145058114+Sandroz4@users.noreply.github.com>
2025-11-15 12:44:49 +0400

    commit

---
+++ Group 37/02 Intermediate Javascript/day 77/classwork/script.js
@@ -0,0 +1,87 @@
+// const numbers = [2, 4, 6, 8, 10]
+
+// numbers.map(function(num) {
+//   console.log(num*2)
+// })
+
+
+// const names = ['alex', 'mate', 'oliko', 'tika', 'nikolozi']
+
+// names.map(function (name, i) {
+
+
+
+
+
+
+
+//     // console.log(name.toUpperCase())
+//     // console.log(name.length)
+//     // console.log('hello ' + name)
+
+//     // console.log('index ' + i + ' is value: ' + name )
+// })
+
+// let name = 'sandro'
+
+// console.log(name.toUpperCase())
+
+
+// const people = [['sandro', 'mate'], ['giorgi', 'nika']]
+
+
+// people.map(function(person, index) {
+//     person.map(function(human, i) {
+//         console.log(human, i)
+//     })
+// })
+
+// const person = {
+//     name: 'giorgi', 
+//     age: 15
+// }
+
+
+// console.log(person.name, person.age)
+
+// const people = [
+//     { name: 'giorgi', age: 15},
+//     { name: 'sandro', age: 15},
+//     { name: 'dito', age: 20}
+// ]
+
+
+// people.map(function(person) {
+//     if (person.age >= 18) {
+//         console.log(person.name)
+//     } else {
+//         console.log(person.name + ' is not old enough')
+//     }
+// })
+
+// console.log(people[0])
+
+
+// const numbers = [1, 2, 3, 4, 5, 6, 7]
+
+// numbers.map(function (num) {
+//     if (num %2 === 0) {
+//         console.log(num + ' is even')
+//     } else {
+//         console.log(num + ' is odd')
+//     }
+// })
+
+
+// const numbers = [2, 4, 6, 8]
+
+// numbers.map(function (num) {
+//     console.log(num)
+// })
+
+
+// numbers.map(num => console.log(num))
+
+// numbers.map( (num) => {console.log(num)})
+
+// numbers.map( (num, index) => console.log(num, index))
