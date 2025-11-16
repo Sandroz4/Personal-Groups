a7ef983d54b419bf07d6999324f2a9cb711b8be0
Sandro Zabakhidze <145058114+Sandroz4@users.noreply.github.com>
2025-11-16 20:45:32 +0400

    commit

---
+++ Group 44,45,46/day 125/classwork/script.js
@@ -0,0 +1,95 @@
+// 2. შექმენით საიტი, სადაც გექნებათ პარაგრაფი, ცალკე დივი და მასში კი ღილაკი, 
+// ჯავასკრიპტის მეშვეობით დივში არსებული ღილაკი ჩაანაცვლეთ პარაგრაფით.
+
+
+
+
+
+// let btn = document.getElementById('btn')
+
+
+// function replaceChild() {
+//     parent.replaceChild(paragraph, child)
+//     console.log(5)
+// }
+
+
+// btn.addEventListener('click', replaceChild)
+
+
+// let paragraph = document.querySelector('p')
+// let parent = document.querySelector('div')
+
+// parent.removeChild(paragraph)
+
+
+
+
+// parent.appendChild(heading)
+
+// function goa() {
+//     console.log(5)
+// }
+
+
+// let parent = document.querySelector('div')
+// let button = document.querySelector('button')
+
+// let paragraph = document.createElement('p')
+// paragraph.textContent = 'new paragraph'
+
+
+// parent.replaceChild(paragraph, button)
+
+// let parent = document.querySelector('div')
+// let button = document.querySelector('button')
+
+// let paragraph = document.createElement('p')
+// let text = document.createTextNode('Hello World')
+// paragraph.appendChild(text)
+
+
+// parent.replaceChild(paragraph, button)
+
+
+
+
+// // .includes()
+// console.log('hello'.includes('he'))
+// console.log([1, 2, 3].includes(2))
+
+
+// // .indexOf()
+// console.log('hello'.indexOf('h'))
+// console.log([1, 2, 3].indexOf(2))
+
+// // .slice()
+// console.log('hello'.slice(0, 2))
+// console.log([1,2,3].slice(0, 1))
+
+// // .split()
+// console.log('a,b,c'.split(','))
+// console.log('2025-8-19'.split('-'))
+
+// // .join()
+// console.log(['a', 'b', 'c'].join('-'))
+
+// // .trim()
+// console.log('   hello    '.trim())
+// console.log('  a   b   c   '.trim())
+
+// // .repeat()
+// console.log('ha'.repeat(3))
+
+// .replace() & replaceAll()
+// console.log('banana'.replaceAll('na', 'NA'))
+// console.log('11/16/2025'.replace('/', '-'))
+
+
+// // .reverse()
+// console.log([1,2,3].reverse())
+
+
+// .sort()
+// console.log([1,4, 2].sort())
+// console.log(['100', '5555', '5'].sort())
