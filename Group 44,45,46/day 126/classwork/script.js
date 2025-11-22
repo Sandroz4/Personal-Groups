38357fe48db5f0795aaf4fb96180c7daa42eb43b
Sandro Zabakhidze <145058114+Sandroz4@users.noreply.github.com>
2025-11-22 13:51:45 +0400

    commit

---
+++ Group 44,45,46/day 126/classwork/script.js
@@ -0,0 +1,44 @@
+// let mixedArr = [1, 2, '3', '4']
+
+// // const result = mixedArr.map(item => {
+// //     return item
+// // }
+// // )
+
+// // console.log(result)
+
+// let sum = 0 // 3 3 4
+
+// mixedArr.map(function (item) {
+//     sum += Number(item)
+// })
+
+// mixedArr.map(item => sum += Number(item))
+
+// console.log(sum)
+
+
+// let mixedArr = [122002002, 122002002, 122002002, 122002002]
+
+
+// console.log(Math.min(...mixedArr))
+
+// console.log(...mixedArr)
+
+
+// let min = mixedArr[1]
+
+// mixedArr.map(item => {
+//     if (item < min) {
+//         min = item
+//     }
+// })
+// console.log(min)
+
+
+
+// const numbers = [1, 5, 10, 15, 20]
+
+// const newNumbers = numbers.filter(item => item > 10)
+
+// console.log(newNumbers)
