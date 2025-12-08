3d0349d40b69d86bd3dfc87518cbfb0b30195c29
Sandro Zabakhidze <145058114+Sandroz4@users.noreply.github.com>
2025-12-08 23:01:56 +0400

    commit

---
+++ Group 44,45,46/day 131/classwork/script.js
@@ -0,0 +1,85 @@
+// მოცემულია ასო და უნდა გავიგოთ მისი პოზიცია ანბანში
+
+let alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+let char = 'z'
+
+console.log(alphabet.indexOf(char) + 1)
+
+
+
+// // როგორ შევაბრუნოთ სიტყვა
+
+// let word = 'hello'
+// console.log(word.split('').reverse().join(''))
+
+
+// // შევამოწმოთ პალინდრომია თუ არა
+
+// let firstName = 'dad'
+// let reversedFirstName = firstName.split('').reverse().join('')
+
+// if (firstName ===  reversedFirstName) {
+//     console.log(true)
+// } else {
+//     console.log(false)
+// }
+
+
+
+
+// კონსოლში გვინდა გამოვიტანოთ რიცხვი 5, რომ რიცხვებს ვერ დავწერთ კოდში
+
+// let name = 'wordyx'
+// console.log(name.indexOf('x'))
+
+// let list = [1, 3, 5]
+// console.log(list.size())
+
+
+// for (let i = null; i < sentence.length; i++) {
+//     console.log(i, sentence[i])
+// }
+
+// console.log('aaaaa'.length)
+
+// for (let i = 0; i < sentence.length; i++) {
+//     console.log(i, sentence[i])
+// }
+
+
+// თუ დიდია ასო - დავაპატარავოთ,
+// თუ პატარაა ასო - გავადიდოთ
+
+// let word = 'hEllo WORld'
+// let word2 = ''
+
+// for (let i = 0; i < word.length; i++) {
+//     if (word[i] === word[i].toUpperCase()) {
+//         word2 += word[i].toLowerCase()
+//     } else {
+//         word2 += word[i].toUpperCase()
+//     }
+// }
+
+// console.log(word2)
+
+
+
+
+
+
+// let letter = 'a'
+
+
+// if (letter == letter.toUpperCase()) {
+//     console.log(true)
+// } else {
+//     console.log(false)
+// }
+
+// console.log('5' == 5) true
+// console.log('5' === 5) false
+
+
+// console.log(letter.toUpperCase())
