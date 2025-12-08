d58b4c1e0098633d15cb71adb3f63afeda940650
Sandro Zabakhidze <145058114+Sandroz4@users.noreply.github.com>
2025-12-08 20:51:01 +0400

    commit

---
+++ classwork/script.js
@@ -0,0 +1,15 @@
+// let name = 'gio' // ცვლადი
+// const age = 15   // კონსტანტა
+
+// console.log(name)
+
+// console.log()
+
+{/* <p id="para">hello</p> */}
+
+let greet = document.getElementById('para')
+
+console.log(greet)
+
+// greet.textContent = 'goa'
+
