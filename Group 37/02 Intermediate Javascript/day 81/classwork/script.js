35a7ff6b71f44016331b37e3c8d90584b5eb1a15
Sandro Zabakhidze <145058114+Sandroz4@users.noreply.github.com>
2025-12-09 22:37:56 +0400

    commit

---
+++ Group 37/02 Intermediate Javascript/day 81/classwork/script.js
@@ -0,0 +1,96 @@
+// // კონსოლში გვინდა რომ გამოვიტანოთ ციფრი 5, მაგრამ კოდში ვერ გამოვიყენებთ ციფრებს
+
+// // let list = ['a', 'b', 'c', 'd', 'e', 'f']
+
+// // list.map( (item, index) => {
+// //     if (item === 'f') {
+// //         console.log(index)
+// //     }
+// // })
+
+
+// console.log('hello'.length)
+
+
+
+// პატარა ასოები უნდა გავადიდოთ და დიდები დავაპატარავოთ
+
+// let greet = 'HellO WOrld'
+// let greetSplitted = greet.split('')
+
+// let res = ''
+
+
+// greetSplitted.map( (char) => {
+//     if (char === char.toUpperCase()) {
+//         res += char.toLowerCase()
+//     } else {
+//         res += char.toUpperCase()
+//     }
+// })
+
+// console.log(res)
+
+
+// greeting.map(char => {
+//     if (char === char.toUpperCase()) {
+//         res += char.toLowerCase()
+//     } else {
+//         res += char.toUpperCase()
+//     }
+// })
+
+// console.log(res)
+
+
+
+
+
+
+
+
+// if (letter === letter.toUpperCase()) {
+//     console.log(true)
+// }
+
+
+// return letter === letter.toUpperCase()
+
+// // შევაბრუნოთ სტრინგი
+
+// let name = 'mate is a student'
+
+// console.log(name.split(' ').reverse().join(' '))
+
+
+// let name = 'goa'
+
+// console.log(name.endsWith('g'))
+
+// const fruits = ["Banana", "Orange", "Lemon", "Apple", "Mango"];
+// const citrus = fruits.slice(0, -1);
+
+// console.log(citrus)
+
+// let list = [1, 2, 3, 4]
+
+// result = list.filter( item => item > 2)
+
+// console.log(result)
+
+// result = list.map(item => item + 1)
+
+// console.log(result)
+
+// list.map(item => console.log(item))
+
+
+// let zuraDavitadze = ['mate', 'dito', 'oliko', 'tika', 'nikolozi', 'alex']
+
+// let result = zuraDavitadze.filter( name => {
+//     name.length > 4
+// })
+
+// let result = zuraDavitadze.filter( name => name.length > 4)
+
+// console.log(result)
