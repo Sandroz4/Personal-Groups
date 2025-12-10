0d508d5014bba8f97449c52b983a7ad4ccad1f67
Sandro Zabakhidze <145058114+Sandroz4@users.noreply.github.com>
2025-12-10 20:56:45 +0400

    commit

---
+++ Extra Groups/01/classwork/script.js
@@ -0,0 +1,38 @@
+// let inp = document.querySelector('input')
+
+// function logFive() {
+//     console.log(5)
+// }
+
+// inp.addEventListener('click', logFive)
+
+// inp.addEventListener('onchange', )
+
+// // კონსოლში გვინდა გამოვიტანოთ 5, მაგრამ კოდში ვერ გამოვიყენებთ ციფრებს
+
+// console.log('goala'.length)
+
+// // თუ ასო დიდია გამოვიტანოთ true, თუ პატარაა false
+
+// let letter = 'A'
+
+// if (letter.toUpperCase() === letter) {
+//     console.log(true)
+// } else {
+//     console.log(false)
+// }
+
+// // სიტყვაში პატარა ასოები ჩავანაცვლოთ დიდით და დიდი ასოები ჩავანაცვლოთ პატარებით
+
+// let name = 'gRouP'
+// let res = ''
+
+// for (let i = 0; i < name.length; i++) {
+//     if (name[i].toUpperCase() === name[i]) {
+//         res += name[i].toLowerCase()
+//     } else {
+//         res += name[i].toUpperCase()
+//     }
+// }
+
+// console.log(res)
