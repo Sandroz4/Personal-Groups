5bb96991cd4777d85f3145daf54a3e13df6947bb
Sandro Zabakhidze <145058114+Sandroz4@users.noreply.github.com>
2026-01-03 13:29:23 +0400

    commit

---
+++ Group 44,45,46/02 Intermediate Javascript/day 137/classwork/script.js
@@ -0,0 +1,69 @@
+// let interval = setInterval(
+//     function() {
+//         console.log('helo world')
+//     },  1000)
+
+
+
+// setInterval(
+//     function () {
+//         console.log(5)
+//     }, 2000
+// )
+
+
+// setInterval(() => {
+//     console.log(5)
+// })
+
+
+// let interval = setInterval(() => {
+//     console.log('hello world')
+// }, 1000)
+
+
+// clearInterval(interval)
+
+
+
+// let count = 5
+
+// const plusInterval = setInterval(() => {
+//     count += 1
+//     console.log(count)
+
+
+//     if (count >= 10) {
+//         clearInterval(plusInterval)
+//         const minusInterval = setInterval(() => {
+            
+//             count -= 1
+//             console.log(count)
+
+//             if (count <= -1) {
+//                 clearInterval(minusInterval)
+//             }
+//         },2000)
+//     } 
+// }, 1000)
+
+
+
+    // if (count > 10) {
+    //     clearInterval(countInterval)
+    // }
+
+
+// const varTime = setTimeout(() => {
+//     console.log('hello')
+// }, 5000)
+
+// clearTimeout(varTime)
+
+
+
+// const btn = document.querySelector('button')
+
+// btn.addEventListener('click', setTimeout(() => {
+//     alert('Time is up')
+// }, 3000))
