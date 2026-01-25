d7adcc72a38ac387ab163726ffb811ec22fdd4ea
Sandro Zabakhidze <145058114+Sandroz4@users.noreply.github.com>
2026-01-25 20:43:26 +0400

    commit

---
+++ Group 44,45,46/02 Intermediate Javascript/day 144/classwork/script.js
@@ -0,0 +1,94 @@
+// function double(n) {
+//     return n * 2
+// }
+
+// console.log(double(3))
+
+
+// const numbers = [1,2,3,4,5]
+
+// numbers.map( num => {
+//     console.log('helllo')
+// })
+
+
+// function double(n) {
+//     return n * 2
+// }
+
+// const double = n => n * 2
+
+// console.log(double(16))
+
+
+
+
+
+
+// function add(a, b) {
+//     return a + b
+// }
+
+// console.log(add(5, 10))
+
+// const add = (a, b) => {
+//     console.log(a + b)
+// }
+
+// add(10, 5)
+
+// console.log(add(5, 10))
+
+
+// function isPositive(n) {
+//     if (n > 0) {
+//         return true
+//     } else {
+//         return false
+//     }
+// }
+
+
+// const isPositive = n => {
+//     return n > 0
+// }
+
+
+// const getFullName = (first, last) => {
+//     return `${first} ${last}`
+// }
+
+// console.log(getFullName('saba', 'sanaia'))
+
+
+
+// const user = {
+//     name: 'Saba',
+//     age: 21
+// };
+
+// // console.log(user.age, user.name)
+
+// const { name, age } = user
+
+
+// console.log(name, age)
+
+// const car = {brand: 'mustang', year: 2020}
+
+// const { brand, year } = car
+
+// console.log(brand, year)
+
+
+// const settings = {
+//     theme: 'dark',
+//     width: '100px'
+// }
+
+// const { theme, width } = settings
+
+// console.log(width)
+
+// console.log(settings.theme)
+
