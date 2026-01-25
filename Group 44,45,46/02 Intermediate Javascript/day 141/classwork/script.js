f89d61f879c021a3d74c90cdcb724535b6330197
Sandro Zabakhidze <145058114+Sandroz4@users.noreply.github.com>
2026-01-25 19:06:28 +0400

    commit

---
+++ Group 44,45,46/02 Intermediate Javascript/day 141/classwork/script.js
@@ -0,0 +1,21 @@
+const btn1 = document.getElementById("btn1")
+const btn2 = document.getElementById("btn2")
+const btn3 = document.getElementById("btn3")
+const btn4 = document.getElementById("btn4")
+
+btn1.addEventListener("click",() =>{
+    const sound1 = getElementById("sound1")
+    sound1.play()
+})
+
+btn2.addEventListener("click",() =>{
+    
+})
+
+btn3.addEventListener("click",() =>{
+    
+})
+
+btn4.addEventListener("click",() =>{
+    
+})
